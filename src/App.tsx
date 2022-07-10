a027e6466e135a41a4f901e2c5e1fc3b585b1e85
SNUPI <joohaem@daum.net>
2022-07-11 01:50:47 +0900

    [ feat ] 반응형 임시 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+
 import Router from "./Router";
 
 export default function App() {
-  return <Router />;
+  return (
+    <St.MobileContainer>
+      <Router />
+    </St.MobileContainer>
+  );
 }
+
+const St = {
+  MobileContainer: styled.div`
+    margin: 0 auto;
+
+    @media screen and (min-width: 48rem), screen and (min-height: 48rem) and (orientation: landscape) {
+      width: 36rem;
+    }
+  `,
+};
