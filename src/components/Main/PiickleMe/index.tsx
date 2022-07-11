262f1a0f86fe397a20cf26d8aee6337100c36e44
JY <pianowell96@gmail.com>
2022-07-12 03:45:12 +0900

    [ fix ] 프로필 이미지 삽입

--- src/components/Main/PiickleMe/index.tsx
+++ src/components/Main/PiickleMe/index.tsx
@@ -1,26 +1,24 @@
-import { element } from "prop-types";
-
 import { IcVoteImg1, IcVoteImg2 } from "../../../asset/icon";
 import { St } from "./style";
 
 const voteData = [
   {
     id: 0,
-    profileImg: IcVoteImg1,
+    profileImg: <IcVoteImg2 aria-label="사용자 프로필 사진" />,
     contentText: "인간관계에서 가장 중요하게 생각하는 것은?",
     subText: "텍스트텍스트",
     voteBtn: "투표하기",
   },
   {
     id: 1,
-    profileImg: IcVoteImg2,
+    profileImg: <IcVoteImg1 aria-label="사용자 프로필 사진" />,
     contentText: "인간관계에서 가장 중요하게 생각하는 것은?",
     subText: "텍스트텍스트",
     voteBtn: "투표하기",
   },
   {
     id: 2,
-    profileImg: IcVoteImg2,
+    profileImg: <IcVoteImg1 aria-label="사용자 프로필 사진" />,
     contentText: "인간관계에서 가장 중요하게 생각하는 것은?",
     subText: "텍스트텍스트",
     voteBtn: "투표하기",
@@ -37,12 +35,12 @@ export default function PiickleMe() {
       <St.RepresentVoteContentConTainer>
         <St.VoteContentText isFirst={true}>{representVoteData.contentText}</St.VoteContentText>
         <St.VoteContentSubText isFirst={true}>{representVoteData.discription}</St.VoteContentSubText>
-        <St.FirstGoVoteBtn>투표하기</St.FirstGoVoteBtn>
+        <St.RepresentGoVoteBtn>투표하기</St.RepresentGoVoteBtn>
       </St.RepresentVoteContentConTainer>
       {voteData.map((element) => {
         return (
           <St.VoteContent key={element.id}>
-            <St.VoteProfileImage></St.VoteProfileImage>
+            {element.profileImg}
             <St.VoteTextContainer>
               <St.VoteContentText isFirst={false}>{element.contentText.slice(0, 18)}...</St.VoteContentText>
               <St.VoteContentSubText isFirst={false}>{element.subText}</St.VoteContentSubText>
