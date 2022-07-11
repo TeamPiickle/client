18e24ffe7bd69ee5b40be9848764bf2f59f8b24a
SNUPI <joohaem@daum.net>
2022-07-11 16:47:46 +0900

    [ feat ] add headingTitles

---
+++ src/core/main/headingTitles.ts
@@ -0,0 +1,25 @@
+type HeadingTitle = {
+  title: string;
+  content: string;
+  isMoreBtn: boolean;
+};
+
+const headingTitles: HeadingTitle[] = [
+  {
+    title: "이번 달 Best Piickle",
+    content: "한 달 동안 가장 많은 마이피클을 달성한 주제들이에요",
+    isMoreBtn: false,
+  },
+  {
+    title: "Mood Piickle",
+    content: "더욱 쉽게 피클을 즐길 수 있는 방법!\n지금 나의 무드에 맞는 카테고리를 골라보세요",
+    isMoreBtn: true,
+  },
+  {
+    title: "이번 달 Best Piickle",
+    content: "한 달 동안 가장 많은 마이피클을 달성한 주제들이에요",
+    isMoreBtn: false,
+  },
+];
+
+export default headingTitles;
