cd46a9c92e6eeb93ae33bf5118107311ad2b524a
SNUPI <joohaem@daum.net>
2022-07-11 21:16:18 +0900

    [ etc ] 미사용 모듈 제거

--- src/components/Main/common/HeadingTitleContainer/style.ts
+++ src/components/Main/common/HeadingTitleContainer/style.ts
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const St = {
   Container: styled.header`
