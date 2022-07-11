d016ec21babf680d31d60a9f167aaad7e7e7d059
NYeonK <skdus728@gmail.com>
2022-07-11 20:25:44 +0900

    [ feat ] react slick 기능 구현

--- src/components/Main/Banner/index.tsx
+++ src/components/Main/Banner/index.tsx
@@ -1,7 +1,43 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import React, { Component } from "react";
+import Slider from "react-slick";
 
 import { St } from "./style";
 
 export default function Banner() {
-  return <div>Banner</div>;
+  const settings = {
+    arrows: false,
+    dots: false,
+    infinite: false,
+    slidesToShow: 1.5,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <h2> Single Item</h2>
+      <Slider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
 }
