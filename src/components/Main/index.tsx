c497103a7f89f27ad3ea4fc04d47712f0688e84b
JY <pianowell96@gmail.com>
2022-07-12 16:36:51 +0900

    [ feat ] 컴포넌트에 타이클 컴포넌트 병합

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
 
 import BestPiickle from "./BestPiickle";
+import HeadingTitleContainer from "./common/HeadingTitleContainer";
+import CTABtn from "./CTABtn";
 import PiickleMe from "./PiickleMe";
 
 export default function Main() {
   return (
     <main>
-      <PiickleMe />
       <BestPiickle />
+      <HeadingTitleContainer
+        headingTitles={{
+          title: "Mood Piickle",
+          content: "더욱 쉽게 피클을 즐길 수 있는 방법!\n지금 나의 무드에 맞는 카테고리를 골라보세요",
+          isMoreBtn: true,
+        }}
+      />
+      <PiickleMe />
+      <CTABtn />
     </main>
   );
 }
