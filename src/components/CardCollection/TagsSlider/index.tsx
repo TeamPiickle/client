2f43e7b59a762583112d3ea05acd8bc0a41d647a
SNUPI <joohaem@daum.net>
2022-07-13 01:18:34 +0900

    [ etc ] 미사용 모듈 제거

--- src/components/CardCollection/TagsSlider/index.tsx
+++ src/components/CardCollection/TagsSlider/index.tsx
@@ -1,18 +1,9 @@
-import Slider from "react-slick";
-
 import Tag from "../Tag";
 import { St } from "./style";
 
 interface TagsSliderProps {
   tags: string[];
 }
-
-const sliderSettings = {
-  arrows: false,
-  dots: false,
-  infinite: false,
-  slidesToShow: 3.5,
-};
 
 export default function TagsSlider(props: TagsSliderProps) {
   const { tags } = props;
