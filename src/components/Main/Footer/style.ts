c78337f05a928f1d4a7fb9da4d358dcb18c03cc2
NYeonK <skdus728@gmail.com>
2022-07-13 12:14:25 +0900

    [ feat ] footer 구현

--- src/components/Main/Footer/style.ts
+++ src/components/Main/Footer/style.ts
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
 export const St = {
-  Footer: styled.section``,
+  Footer: styled.section`
+    display: flex;
+    margin-top: 12rem;
+    height: 22rem;
+
+    background: #f1f1f1;
+  `,
+
+  BasicInfo: styled.ul``,
+  BasicInfoTitle: styled.li``,
+
+  ServiceInfo: styled.ul``,
+  ServiceInfoTitle: styled.li``,
+  ServiceInfoDetail: styled.li``,
 };
