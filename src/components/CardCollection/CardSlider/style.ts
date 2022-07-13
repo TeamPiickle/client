1a8777a699384bedfe1f4cf886ae368de5c5e2d7
SNUPI <joohaem@daum.net>
2022-07-13 13:54:05 +0900

    [ feat ] scale

--- src/components/CardCollection/CardSlider/style.ts
+++ src/components/CardCollection/CardSlider/style.ts
@@ -12,26 +12,37 @@ import styled from "styled-components";
 
 export const St = {
   Wrapper: styled.section`
-    & .slick-slide article {
-      width: calc(100vw - 5.6rem);
-      height: calc(100vh - 10.5rem);
-      /* height: calc(100vh - 14.8rem); */
+    & .slick-slide {
+      width: 30.5rem;
+      height: calc(100vh - 14.8rem);
+    }
 
+    & .slick-slide > div {
+      height: 100%;
       margin: 0 1.2rem;
+    }
 
-      background: rgba(255, 255, 255, 0.5);
-      backdrop-filter: blur(1.2rem);
+    & .slick-slide article {
+      height: 100%;
+      /* width: calc(100vw - 5.6rem); */
+      /* height: calc(100vh - 10.5rem); */
+
       /* background: linear-gradient(0deg, rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), rgba(211, 245, 232, 0.5);
       backdrop-filter: blur(1.2rem); */
     }
 
     & .slick-slide.slick-active article {
+      background: rgba(255, 255, 255, 0.5);
+      backdrop-filter: blur(1.2rem);
       /* height: calc(100vh - 10.5rem); */
-
-      /* background: all 0.5s; */
+      transform: scale(1.05);
+      transition: all 0.5s;
     }
 
     & .slick-track {
+      height: calc(100vh - 10.5rem);
+      /* height: calc(100vh - 14.8rem); */
+
       display: flex;
       align-items: center;
     }
