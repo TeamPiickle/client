82298a7bb7711f23867a36ddf1059f0538dc3391
JY <pianowell96@gmail.com>
2022-07-15 04:30:22 +0900

    [ feat ] 클릭한 항목 변화

--- src/components/Vote/AfterVoteList/index.tsx
+++ src/components/Vote/AfterVoteList/index.tsx
@@ -1,8 +1,15 @@
+import { useEffect, useState } from "react";
+
 import { IcCheck1 } from "../../../asset/icon";
 import { voteContent, voteTitle } from "../../../core/vote/voteContent";
 import { St } from "./style";
 
-export default function AfterVoteList() {
+export default function AfterVoteList(props: any) {
+  const { setIsVoted, setIsSuccess, currentIndex } = props;
+  const cancelVote = () => {
+    setIsSuccess(false);
+    setIsVoted(false);
+  };
   return (
     <>
       <St.VoteOptionContainer>
@@ -11,18 +18,20 @@ export default function AfterVoteList() {
             <St.VoteOptionList key={element.id}>
               <St.VotedDescription>
                 <St.IconTextContainer>
-                  <IcCheck1 />
-                  <St.VoteOptionText>{element.name}</St.VoteOptionText>
+                  {currentIndex === element.id && <IcCheck1 />}
+                  <St.VoteOptionText isSelected={element.id === currentIndex}>{element.name}</St.VoteOptionText>
                 </St.IconTextContainer>
                 <St.VotedPercent>65.6%</St.VotedPercent>
               </St.VotedDescription>
-              <St.VotedProgressBar />
+              <St.VotedProgressBarContainer isSelected={element.id === currentIndex}>
+                <St.VotedProgressBar isSelected={element.id === currentIndex} />
+              </St.VotedProgressBarContainer>
             </St.VoteOptionList>
           );
         })}
       </St.VoteOptionContainer>
       <St.VoteBtnContainer>
-        <St.VoteBtn>투표하기</St.VoteBtn>
+        <St.VoteBtn onClick={cancelVote}>재투표하기</St.VoteBtn>
       </St.VoteBtnContainer>
     </>
   );
