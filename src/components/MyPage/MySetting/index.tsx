472850e85c45cf8f8d57b1de8ef0207b1beee19a
NYeonK <skdus728@gmail.com>
2022-07-14 21:30:55 +0900

    [ feat ] MySetting 컴포넌트 태그 추가>

--- src/components/MyPage/MySetting/index.tsx
+++ src/components/MyPage/MySetting/index.tsx
@@ -2,7 +2,11 @@ import { St } from "./style";
 
 export default function MySetting() {
   const mySetting = ["공지사항", "문의", "팀 정보", "로그아웃", "회원 탈퇴"];
-  const mySettingList = mySetting.map((setting, index) => <St.MySettingList key={index}>{setting}</St.MySettingList>);
+  const mySettingList = mySetting.map((setting, index) => (
+    <St.MySettingList key={index}>
+      <p>{setting}</p>
+    </St.MySettingList>
+  ));
 
   return <St.MySettingContainer>{mySettingList}</St.MySettingContainer>;
 }
--- src/components/MyPage/MySetting/style.ts
+++ src/components/MyPage/MySetting/style.ts
@@ -5,19 +5,22 @@ export const St = {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
-    padding: 0rem 29.7rem 0rem 1.6rem;
-    margin: 3.6rem 0;
-    gap: 2.4rem;
 
-    //width: 36rem;
+    margin: 3.6rem 0;
+    //gap: 2.4rem;
     height: 19.5rem;
   `,
 
-  MySettingList: styled.div`
+  MySettingList: styled.article`
+    //padding: 0rem 29.7rem 0rem 1.6rem;
+    padding-left: 1.6rem;
+    width: 36rem;
+    height: 3.9rem;
+
     letter-spacing: -0.03rem;
     ${({ theme }) => theme.fonts.body6};
     color: ${({ theme }) => theme.colors.bg};
-    width: 36rem;
-    height: 3.9rem;
   `,
+
+  MySettingListDetail: styled.p``,
 };
