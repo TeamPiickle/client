71af9308fd27c1e842409f5b3bd81d9a9ceac31c
JY <pianowell96@gmail.com>
2022-07-15 04:38:34 +0900

    [ deisign ] voteTitle padding 수정

--- src/components/Vote/VoteTitle/style.ts
+++ src/components/Vote/VoteTitle/style.ts
@@ -1,19 +1,18 @@
 import styled from "styled-components";
-
 export const St = {
   VoteTitleContainer: styled.article`
     height: 7.4rem;
     width: 36rem;
 
-    padding-top: 0.2rem;
-    padding-left: 1.6rem;
-    padding-bottom: 4.6rem;
+    padding: 0.2rem 1.6rem 4.6rem;
   `,
 
   VoteTitleText: styled.h1`
     ${({ theme }) => theme.fonts.h1}
 
     color: ${({ theme }) => theme.colors.bg};
+
+    margin-bottom: 0.4rem;
   `,
 
   VoteSubTitleText: styled.h2`
