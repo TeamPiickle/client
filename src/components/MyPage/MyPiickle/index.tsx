cb1fcf77260705e7a4612f0c18f6cef774698d58
NYeonK <skdus728@gmail.com>
2022-07-14 17:30:13 +0900

    [ feat ] MyPiickle 컴포넌트 생성

---
+++ src/components/MyPage/MyPiickle/index.tsx
@@ -0,0 +1,5 @@
+import { St } from "./style";
+
+export default function MyPiickle() {
+  return <St.Container>MyPiickle</St.Container>;
+}
--- src/components/MyPage/index.tsx
+++ src/components/MyPage/index.tsx
@@ -1,10 +1,12 @@
 import MyInfo from "./MyInfo";
+import MyPiickle from "./MyPiickle";
 import { St } from "./style";
 
 export default function MyPage() {
   return (
     <St.MyPage>
       <MyInfo />
+      <MyPiickle />
     </St.MyPage>
   );
 }
