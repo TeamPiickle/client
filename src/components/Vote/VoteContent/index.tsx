7664b70340988ff12cc999a82694cc2e54b5471f
JY <pianowell96@gmail.com>
2022-07-15 04:34:49 +0900

    [ feat ] voteContent 컴포넌트 수정

--- src/components/Vote/VoteContent/index.tsx
+++ src/components/Vote/VoteContent/index.tsx
@@ -1,21 +1,41 @@
 import { useState } from "react";
 
-import { voteContent, voteTitle } from "../../../core/vote/voteContent";
 import AfterVoteList from "../AfterVoteList";
 import BeforeVoteList from "../BeforeVoteList";
 import { St } from "./style";
 
 export default function VoteContent() {
   const [isVoted, setIsVoted] = useState(false);
-
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
   return (
     <St.VoteContentContainer>
       <St.VoteContentTitle>
         민초 vs 반민초 <br />
         여러분의 의견은?
       </St.VoteContentTitle>
-      {isVoted && <BeforeVoteList isVoted={isVoted} setIsVoted={setIsVoted} />}
-      {!isVoted && <AfterVoteList />}
+      {!isSuccess && (
+        <BeforeVoteList
+          isVoted={isVoted}
+          setIsVoted={setIsVoted}
+          setIsSuccess={setIsSuccess}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+      )}
+      {isSuccess && (
+        <AfterVoteList
+          setIsVoted={setIsVoted}
+          setIsSuccess={setIsSuccess}
+          selectedOption={selectedOption}
+          setSelectedOption={setSelectedOption}
+          currentIndex={currentIndex}
+          setCurrentIndex={setCurrentIndex}
+        />
+      )}
     </St.VoteContentContainer>
   );
 }
