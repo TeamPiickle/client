5e34722f45a36b7c55dade2758f20dfd9d5671ed
JY <pianowell96@gmail.com>
2022-07-14 21:31:42 +0900

    [ feat ] vote 페이지 타이틀 뷰

---
+++ src/components/Vote/VoteTitle/index.tsx
@@ -0,0 +1,10 @@
+import { St } from "./style";
+
+export default function VoteTitle() {
+  return (
+    <St.VoteTitleContainer>
+      <St.VoteSubTitleText>Piickle Me!</St.VoteSubTitleText>
+      <St.VoteSubTitleText>당신의 피클에게 투표하세요</St.VoteSubTitleText>
+    </St.VoteTitleContainer>
+  );
+}
