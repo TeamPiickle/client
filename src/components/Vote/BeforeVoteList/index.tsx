2b8a3ded0b38b03d8666c86002c1860f96e50433
JY <pianowell96@gmail.com>
2022-07-15 04:34:07 +0900

    [ feat ] 투표하기 전 로직수정

--- src/components/Vote/BeforeVoteList/index.tsx
+++ src/components/Vote/BeforeVoteList/index.tsx
@@ -1,17 +1,30 @@
 import { useState } from "react";
 
 import { IcCheck2 } from "../../../asset/icon";
-import { voteContent, voteTitle } from "../../../core/vote/voteContent";
+import { voteContent } from "../../../core/vote/voteContent";
 import { St } from "./style";
 
 export default function BeforeVoteList(props: any) {
-  const { isVoted, setIsVoted } = props;
-  // const [isVoted, setIsVoted] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const { isVoted, setIsVoted, setIsSuccess, setSelectedOption, currentIndex, setCurrentIndex } = props;
 
+  const successVote = () => {
+    if (isVoted === true) {
+      setIsSuccess(true);
+      setSelectedOption(currentIndex);
+    }
+  };
+
+  //함수단위 쪼개기
   const clickHandle = (key: number) => {
-    setIsVoted(!isVoted);
-    setCurrentIndex(key);
+    if (isVoted === true) {
+      if (currentIndex === key) {
+        setIsVoted(false);
+      }
+      setCurrentIndex(key);
+    } else {
+      setIsVoted(!isVoted);
+      setCurrentIndex(key);
+    }
   };
   return (
     <>
@@ -29,7 +42,7 @@ export default function BeforeVoteList(props: any) {
         })}
       </St.VoteOptionContainer>
       <St.VoteBtnContainer>
-        <St.VoteBtn>투표하기</St.VoteBtn>
+        <St.VoteBtn onClick={successVote}>투표하기</St.VoteBtn>
       </St.VoteBtnContainer>
     </>
   );
