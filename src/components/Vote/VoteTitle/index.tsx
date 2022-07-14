8ca615f537b2acb6b56addfaf7c42bfadcc4f8c3
JY <pianowell96@gmail.com>
2022-07-15 04:36:40 +0900

    [ fix ] 컴포넌트 철자수정

--- src/components/Vote/VoteTitle/index.tsx
+++ src/components/Vote/VoteTitle/index.tsx
@@ -3,7 +3,7 @@ import { St } from "./style";
 export default function VoteTitle() {
   return (
     <St.VoteTitleContainer>
-      <St.VoteSubTitleText>Piickle Me!</St.VoteSubTitleText>
+      <St.VoteTitleText>Piickle Me!</St.VoteTitleText>
       <St.VoteSubTitleText>당신의 피클에게 투표하세요</St.VoteSubTitleText>
     </St.VoteTitleContainer>
   );
