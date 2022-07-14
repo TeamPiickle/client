36754682a26e7fbeca31f65c269e7e9af7240b12
JY <pianowell96@gmail.com>
2022-07-15 04:31:45 +0900

    [ design ] 투표한 후 스타일 수정

--- src/components/Vote/AfterVoteList/style.ts
+++ src/components/Vote/AfterVoteList/style.ts
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+import { IcCheck1 } from "../../../asset/icon";
+
 export const St = {
   VoteOptionContainer: styled.ul`
     width: 27.2rem;
@@ -30,26 +32,40 @@ export const St = {
     display: flex;
   `,
 
-  CheckIcon: styled.div``,
+  IcCheck1: styled(IcCheck1)<{ isSelected: boolean }>`
+    display: ${({ isSelected }) => (!isSelected ? "none" : "block")};
+  `,
 
-  VoteOptionText: styled.strong`
-    ${({ theme }) => theme.fonts.btn2}
-    color: ${({ theme }) => theme.colors.gray800};
+  VoteOptionText: styled.strong<{ isSelected: boolean }>`
+    ${({ theme }) => theme.fonts.btn2};
+    color: ${({ theme, isSelected }) => (isSelected ? theme.colors.gray800 : theme.colors.gray600)};
 
     margin-left: 0.4rem;
   `,
 
   VotedPercent: styled.strong``,
 
-  VotedProgressBar: styled.div`
+  VotedProgressBarContainer: styled.div<{ isSelected: boolean }>`
     width: 27.2rem;
     height: 1rem;
 
     border-radius: 4rem;
 
-    background-color: ${({ theme }) => theme.colors.green};
+    background-color: ${({ theme }) => theme.colors.gray100};
+    box-shadow: inset 0px 1px 1px rgba(0, 0, 0, 0.25);
+    opacity: ${({ isSelected }) => (isSelected ? "rgba(0, 0, 0, 0.25)" : "1")};
 
     margin-top: 0.8rem;
+
+    overflow: hidden;
+  `,
+
+  VotedProgressBar: styled.div<{ isSelected: boolean }>`
+    height: 1rem;
+    width: 65%;
+
+    background-color: ${({ theme, isSelected }) => (isSelected ? theme.colors.green : theme.colors.sub_green1)};
+    opacity: 1;
   `,
 
   VoteBtnContainer: styled.div`
