fe9bcd0c49b32769f555bba434a98421c0180b11
NYeonK <skdus728@gmail.com>
2022-07-14 17:17:19 +0900

    [ design ] MyInfo css 완성

--- src/components/MyPage/MyInfo/index.tsx
+++ src/components/MyPage/MyInfo/index.tsx
@@ -3,11 +3,11 @@ import { St } from "./style";
 export default function MyInfo() {
   return (
     <St.Container>
-      <St.ProfileImage>사진</St.ProfileImage>
+      <St.ProfileImage></St.ProfileImage>
       <St.Profile>
         <St.ProfileNickname>
-          <p>피클깡패</p>
-          <button>닉네임 수정</button>
+          <St.ProfileMyNickname>피클깡패</St.ProfileMyNickname>
+          <St.ProfileNicknameEdit>닉네임 수정</St.ProfileNicknameEdit>
         </St.ProfileNickname>
         <St.ProfileEmail>piickle_people@piickle.com</St.ProfileEmail>
       </St.Profile>
--- src/components/MyPage/MyInfo/style.ts
+++ src/components/MyPage/MyInfo/style.ts
@@ -5,24 +5,76 @@ export const St = {
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding: 24px 124px 36px 16px;
-    gap: 24px;
+    padding: 2.4rem 0 3.6rem 1.6rem;
+    gap: 2.4rem;
 
-    width: 360px;
-    height: 118px;
+    width: 36rem;
+    height: 11.8rem;
   `,
 
-  ProfileImage: styled.div``,
+  ProfileImage: styled.div`
+    width: 5.8rem;
+    height: 5.8rem;
+    border-radius: 50%;
 
-  Profile: styled.div``,
+    background-color: black;
+  `,
+
+  Profile: styled.div`
+    align-items: center;
+    width: auto;
+    height: 4.7rem;
+  `,
 
   ProfileNickname: styled.div`
     display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0rem;
+    gap: 0.8rem;
+
+    width: auto;
+    height: 2.6rem;
+
+    opacity: 0.8;
   `,
 
-  ProfileMyNickname: styled.p``,
+  ProfileMyNickname: styled.p`
+    width: auto;
+    height: 2.6rem;
 
-  ProfileNicknameEdit: styled.button``,
+    ${({ theme }) => theme.fonts.h1};
+    color: ${({ theme }) => theme.colors.bg};
+    letter-spacing: -0.03em;
+  `,
 
-  ProfileEmail: styled.div``,
+  ProfileNicknameEdit: styled.button`
+    display: flex;
+    flex-direction: row;
+    align-items: flex-start;
+    padding: 0.3rem 0.8rem;
+    gap: 0.8rem;
+
+    width: 6rem;
+    height: 2rem;
+
+    background-color: ${({ theme }) => theme.colors.gray300};
+    color: ${({ theme }) => theme.colors.black};
+
+    border-radius: 0.7rem;
+
+    font-family: "Pretendard";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 1rem;
+    line-height: 140%;
+
+    letter-spacing: -0.04rem;
+  `,
+
+  ProfileEmail: styled.div`
+    letter-spacing: -0.03rem;
+    ${({ theme }) => theme.fonts.body2};
+    color: ${({ theme }) => theme.colors.gray600};
+  `,
 };
