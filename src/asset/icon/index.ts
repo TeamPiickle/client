84acb29d715ad8de3b9a4f04fe4b58b62d3dab6a
JY <pianowell96@gmail.com>
2022-07-14 14:15:15 +0900

    [ design ] 이미지 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,4 +1,6 @@
+export { ReactComponent as IcCloseBtn } from "./closeBtn.svg";
 export { ReactComponent as IcHamburger } from "./hamburger.svg";
 export { ReactComponent as IcLogo } from "./logo.svg";
+export { ReactComponent as IcMenuBarImg } from "./MenuBar Profile.svg";
 export { ReactComponent as IcVoteImg1 } from "./voteImg1.svg";
 export { ReactComponent as IcVoteImg2 } from "./voteImg2.svg";
