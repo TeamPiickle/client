9ce671d8c3ee6f0021eefbc5df6c0fc2d0335e35
JY <pianowell96@gmail.com>
2022-07-15 23:43:01 +0900

    [ feat ] category뷰 구현

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,9 +1,29 @@
-import Header from "../common/Header";
+import styled from "styled-components";
 
+import { CategoryBanner } from "../../asset/image";
+import { HeadingTitle } from "../../core/main/headingTitles";
+import Header from "../common/Header";
+import HeadingTitleContainer from "../common/HeadingTitleContainer";
+import CategoryContents from "./CategoryContents";
 export default function Category() {
+  const categoryTitles: HeadingTitle = {
+    title: "Mood Piickle",
+    content: "더욱 쉽게 피클을 즐길 수 있는 방법!\n지금 나의 무드에 맞는 카테고리를 골라보세요",
+    isMoreBtn: false,
+  };
   return (
     <main>
       <Header />
+      <St.CategoryBanner src={CategoryBanner} />
+      <HeadingTitleContainer headingTitles={categoryTitles} />
+      <CategoryContents />
     </main>
   );
 }
+
+const St = {
+  CategoryBanner: styled.img`
+    width: 36rem;
+    height: 13.6rem;
+  `,
+};
