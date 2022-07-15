bbce2cccde678ab4e87941c2fce50b8d2875dd95
Hennization <shy6295@ewhain.net>
2022-07-15 16:52:21 +0900

    [ feat ] HeadingTitleContainer 생성

--- src/components/Bookmark/index.tsx
+++ src/components/Bookmark/index.tsx
@@ -1,9 +1,18 @@
+import { HeadingTitle } from "../../core/main/headingTitles";
 import Header from "../common/Header";
+import HeadingTitleContainer from "../common/HeadingTitleContainer";
+
+const bookmarkHeadingTitles: HeadingTitle = {
+  title: "MY Piickle",
+  content: "내가 픽한 대화 주제들을 확인해보세요",
+  isMoreBtn: false,
+};
 
 export default function Bookmark() {
   return (
     <main>
       <Header />
+      <HeadingTitleContainer headingTitles={bookmarkHeadingTitles} />
     </main>
   );
 }
