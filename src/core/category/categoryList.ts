8652c19c36597a406cd75baf9f6ad3a0f95a5521
JY <pianowell96@gmail.com>
2022-07-15 23:45:32 +0900

    [ feat ] categoryList 더밈 객체 생성

---
+++ src/core/category/categoryList.ts
@@ -0,0 +1,48 @@
+type categoryListProps = {
+  id: number;
+  title: string;
+  content: string;
+};
+
+export const categoryList: categoryListProps[] = [
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+  {
+    id: 0,
+    title: "소개팅",
+    content: "커플과의 추억을 공유하세요",
+  },
+];
