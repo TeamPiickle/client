0c97265688d98f88d989a58626d553a614bee782
SNUPI <joohaem@daum.net>
2022-07-16 15:56:46 +0900

    [ feat ] add axios interceptors for token

--- src/core/api/common/axios.ts
+++ src/core/api/common/axios.ts
@@ -7,6 +7,15 @@ const real = axios.create({
   },
 });
 
+real.interceptors.request.use((config) => {
+  const headers = {
+    ...config.headers,
+    Authorization: localStorage.getItem("piickle-token"),
+  };
+
+  return { ...config, headers };
+});
+
 export const realReq = {
   // GET은 swr 라이브러리로 대체합니다
   GET_SWR(path: string) {
