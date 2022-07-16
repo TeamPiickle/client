a19af3ffcf25d66a3841a8466b408a8e58321009
SNUPI <joohaem@daum.net>
2022-07-16 14:49:21 +0900

    [ fix ] 에러 수정

--- src/components/MyPage/index.tsx
+++ src/components/MyPage/index.tsx
@@ -1,5 +1,5 @@
+import Footer from "../common/Footer";
 import Header from "../common/Header";
-import Footer from "../Main/Footer";
 import MyInfo from "./MyInfo";
 import MyPiickle from "./MyPiickle";
 import MySetting from "./MySetting";
