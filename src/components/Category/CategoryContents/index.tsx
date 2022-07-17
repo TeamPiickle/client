0e6c90db1e13b8318474cf9dfcaa558a312a7ef5
JY <pianowell96@gmail.com>
2022-07-17 20:52:47 +0900

    [ feat ] mock api 연결

--- src/components/Category/CategoryContents/index.tsx
+++ src/components/Category/CategoryContents/index.tsx
@@ -1,29 +1,35 @@
+import { useEffect, useState } from "react";
+
 import { useCategoryLists } from "../../../core/api/main";
 import { categoryList, gridValue } from "../../../core/category/categoryList";
 import { St } from "./style";
 
 export default function CategoryContents() {
+  type categoryType = {
+    id: string;
+    title: string;
+    content: string;
+  };
   const { categoryLists, isLoading, isError } = useCategoryLists();
   // 이걸로 하면돼!!!!! import 받아온 거 파일 없애주고,
-
-  // 아래 gridValue는 core 폴더에 넣어주면 좋을 것 같아요!~
 
   return (
     <St.FlexContainer>
       <St.CategoryItemContainer>
-        {categoryList.map((item, index) => {
-          return (
-            <St.CategoryItem
-              key={item.id}
-              columnStart={gridValue[index].columnStart}
-              columnEnd={gridValue[index].columnEnd}
-              rowStart={gridValue[index].rowStart}
-              rowEnd={gridValue[index].rowEnd}>
-              <St.CategoryTitle>{item.title}</St.CategoryTitle>
-              <St.CategoryDescription>{item.content}</St.CategoryDescription>
-            </St.CategoryItem>
-          );
-        })}
+        {categoryLists &&
+          categoryLists.data.map((item: any, index: any) => {
+            return (
+              <St.CategoryItem
+                key={index}
+                columnStart={gridValue[index].columnStart}
+                columnEnd={gridValue[index].columnEnd}
+                rowStart={gridValue[index].rowStart}
+                rowEnd={gridValue[index].rowEnd}>
+                <St.CategoryTitle>{item.title}</St.CategoryTitle>
+                <St.CategoryDescription>{item.content}</St.CategoryDescription>
+              </St.CategoryItem>
+            );
+          })}
       </St.CategoryItemContainer>
     </St.FlexContainer>
   );
