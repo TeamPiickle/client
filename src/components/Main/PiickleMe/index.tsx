c8cb5e032099648f4b7881d257526e8ba658d315
Hennization <shy6295@ewhain.net>
2022-07-17 20:54:46 +0900

    [ feat ] Piickle ME Get 연결

--- src/components/Main/PiickleMe/index.tsx
+++ src/components/Main/PiickleMe/index.tsx
@@ -1,24 +1,27 @@
-import { useEffect, useState } from "react";
-
 import { IcVoteImg1, IcVoteImg2 } from "../../../asset/icon";
+import { useBallotLists } from "../../../core/api/main";
 import { headingTitles } from "../../../core/main/headingTitles";
-import { BallotList } from "../../../types/main";
 import HeadingTitleContainer from "../../common/HeadingTitleContainer";
 import { St } from "./style";
 
+type ballot = {
+  _id: string;
+  title: string;
+};
+
 export default function PiickleMe() {
-  const [ballotLists, setBallotLists] = useState<BallotList[]>([]);
+  const { ballotLists, isLoading, isError } = useBallotLists();
 
   return (
     <St.Container>
       <HeadingTitleContainer headingTitles={headingTitles[2]} />
       <St.RepresentVoteContentConTainer>
-        <St.RepresentVoteContentText>{ballotLists[0] && ballotLists[0].title}</St.RepresentVoteContentText>
+        <St.RepresentVoteContentText>{ballotLists && ballotLists.data[0].title}</St.RepresentVoteContentText>
         <St.RepresentVoteContentSubText>현재 가장 참여수가 많은 투표</St.RepresentVoteContentSubText>
         <St.RepresentGoVoteBtn>투표하기</St.RepresentGoVoteBtn>
       </St.RepresentVoteContentConTainer>
-      {ballotLists.slice(1).map((ballot, idx) => {
-        return (
+      {ballotLists &&
+        ballotLists.data.slice(1).map((ballot: ballot, idx: number) => (
           <St.VoteContent key={`${ballot._id}-${idx}`}>
             <St.VoteImgTextWrapper>
               <IcVoteImg2 aria-label="사용자 프로필 사진" />
@@ -28,8 +31,7 @@ export default function PiickleMe() {
             </St.VoteImgTextWrapper>
             <St.GoVoteBtn>투표하기</St.GoVoteBtn>
           </St.VoteContent>
-        );
-      })}
+        ))}
     </St.Container>
   );
 }
