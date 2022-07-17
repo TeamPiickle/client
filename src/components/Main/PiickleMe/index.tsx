9521fa16e64e05405d5bc04830c43f06a2436e85
Hennization <shy6295@ewhain.net>
2022-07-17 22:14:08 +0900

    [ feat ] Piickle me 투표하기 눌렀을 때 해당 투표뷰로 이동

--- src/components/Main/PiickleMe/index.tsx
+++ src/components/Main/PiickleMe/index.tsx
@@ -12,16 +12,22 @@ type ballot = {
 export default function PiickleMe() {
   const { ballotLists, isLoading, isError } = useBallotLists();
 
-  return (
-    <St.Container>
-      <HeadingTitleContainer headingTitles={headingTitles[2]} />
-      <St.RepresentVoteContentConTainer>
-        <St.RepresentVoteContentText>{ballotLists && ballotLists.data[0].title}</St.RepresentVoteContentText>
-        <St.RepresentVoteContentSubText>현재 가장 참여수가 많은 투표</St.RepresentVoteContentSubText>
-        <St.RepresentGoVoteBtn>투표하기</St.RepresentGoVoteBtn>
-      </St.RepresentVoteContentConTainer>
-      {ballotLists &&
-        ballotLists.data.slice(1).map((ballot: ballot, idx: number) => (
+  if (ballotLists)
+    return (
+      <St.Container>
+        <HeadingTitleContainer headingTitles={headingTitles[2]} />
+
+        <St.RepresentVoteContentConTainer>
+          <St.RepresentVoteContentText>{ballotLists && ballotLists.data[0].title}</St.RepresentVoteContentText>
+          <St.RepresentVoteContentSubText>현재 가장 참여수가 많은 투표</St.RepresentVoteContentSubText>
+          <St.RepresentGoVoteBtnWrapper type="button">
+            <St.RepresentGoVoteBtn to={ballotLists && `/ballots/${ballotLists.data[0]._id}`}>
+              투표하기
+            </St.RepresentGoVoteBtn>
+          </St.RepresentGoVoteBtnWrapper>
+        </St.RepresentVoteContentConTainer>
+
+        {ballotLists.data.slice(1).map((ballot: ballot, idx: number) => (
           <St.VoteContent key={`${ballot._id}-${idx}`}>
             <St.VoteImgTextWrapper>
               <IcVoteImg2 aria-label="사용자 프로필 사진" />
@@ -29,9 +35,12 @@ export default function PiickleMe() {
                 <St.VoteContentText>{ballot && ballot.title}</St.VoteContentText>
               </St.VoteTextContainer>
             </St.VoteImgTextWrapper>
-            <St.GoVoteBtn>투표하기</St.GoVoteBtn>
+            <St.GoVoteBtnWrapper type="button">
+              <St.GoVoteBtn to={ballotLists && `/ballots/${ballot._id}`}>투표하기</St.GoVoteBtn>
+            </St.GoVoteBtnWrapper>
           </St.VoteContent>
         ))}
-    </St.Container>
-  );
+      </St.Container>
+    );
+  else return <></>;
 }
