5eaefee4092c9735deefc907b182cf9c93e3efdc
SNUPI <joohaem@daum.net>
2022-07-19 00:32:56 +0900

    [ fix ] 에러 해결

--- src/components/CardCollection/FilterModal/index.tsx
+++ src/components/CardCollection/FilterModal/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { real } from "../../../core/api/cardCollection";
