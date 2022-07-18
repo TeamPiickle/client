f7629b91f31ef929e18cf4a64a639d4698b89219
NYeonK <skdus728@gmail.com>
2022-07-19 00:33:30 +0900

    [ feat ] .svg image 추가

--- src/components/MyPage/MyInfo/index.tsx
+++ src/components/MyPage/MyInfo/index.tsx
@@ -1,5 +1,7 @@
+import { IcChangeProfileBtn } from "../../../asset/icon/index";
 import useUserProfile from "../../../core/api/myPage";
 import { St } from "./style";
+
 export default function MyInfo() {
   const { userProfile, isLoading, isError } = useUserProfile();
 
@@ -8,6 +10,7 @@ export default function MyInfo() {
       {userProfile && (
         <St.Profile>
           <St.ProfileImage src={userProfile.data.profile_image_url} alt="프로필이미지" />
+          <IcChangeProfileBtn />
           <St.ProfileDetail>
             <St.ProfileNickname>
               <St.ProfileMyNickname>{userProfile.data.nickname}</St.ProfileMyNickname>
