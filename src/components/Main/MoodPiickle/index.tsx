ef6e8dbc80841a0c22d2635b3fb117360d8e1ea4
NYeonK <skdus728@gmail.com>
2022-07-18 20:13:02 +0900

    [ feat ] MoodPiickle UI checking

--- src/components/Main/MoodPiickle/index.tsx
+++ src/components/Main/MoodPiickle/index.tsx
@@ -8,18 +8,64 @@ import { St } from "./style";
 export default function MoodPiickle() {
   const [categoryLists, setCategoryLists] = useState<CategoryList[]>([]);
 
+  type Category = {
+    _id: string;
+    title: string;
+    imgurl: string;
+  };
+
+  const category: Category[] = [
+    {
+      _id: "62cbb7d8a8c54f168a6ddfe1",
+      title: "커플",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbcd43a8c54f168a6de005",
+      title: "IF",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbc9caa8c54f168a6de001",
+      title: "친구",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbcd4ea8c54f168a6de007",
+      title: "아이스브레이킹",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbcd2ea8c54f168a6de002",
+      title: "SOPT",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbcd35a8c54f168a6de003",
+      title: "소개팅",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbcd3fa8c54f168a6de004",
+      title: "밸런스게임",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+    {
+      _id: "62cbcd4aa8c54f168a6de006",
+      title: "진지한 대화",
+      imgurl: "https://piiklebucket11.s3.ap-northeast-2.amazonaws.com/%EC%B5%9C%EC%8A%B9%EB%B9%88.png",
+    },
+  ];
+
   return (
     <St.Container>
       <HeadingTitleContainer headingTitles={headingTitles[1]} />
       <St.CategoryWrapper>
-        <St.CategoryOneLine>
-          <St.Category></St.Category>
-          <St.Category></St.Category>
-        </St.CategoryOneLine>
-        <St.CategoryOneLine>
-          <St.Category></St.Category>
-          <St.Category></St.Category>
-        </St.CategoryOneLine>
+        {category.slice(0, 4).map((info) => (
+          <St.CategoryOneLine key={info._id}>
+            <St.Category>{info.title}</St.Category>
+          </St.CategoryOneLine>
+        ))}
       </St.CategoryWrapper>
     </St.Container>
   );
--- src/components/Main/MoodPiickle/style.ts
+++ src/components/Main/MoodPiickle/style.ts
@@ -7,25 +7,32 @@ export const St = {
 
   CategoryWrapper: styled.div`
     padding: 0rem 1.6rem 4.8rem;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.8rem;
+    justify-content: center;
   `,
 
   CategoryOneLine: styled.div`
     display: flex;
-    gap: 0.8rem;
-
+    height: 16rem;
     margin-bottom: 0.8rem;
   `,
 
   Category: styled.article`
+    position: relative;
     flex: 1;
     aspect-ratio: 1 / 1;
+    width: 16rem;
 
     box-sizing: border-box;
     display: flex;
 
-    background: #dbfff1;
-
     border: 0.1rem solid #e0e0e0;
     border-radius: 1.6rem;
+    ${({ theme }) => theme.fonts.body1};
+    color: ${({ theme }) => theme.colors.bg};
+    align-items: center;
+    justify-content: center;
   `,
 };
