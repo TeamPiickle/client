689bd7208f6b160514a3f68756f480f167660170
NYeonK <skdus728@gmail.com>
2022-07-19 00:55:38 +0900

    [ feat ] MyPage 프로필 변경 버튼 구현

--- src/components/MyPage/MyInfo/index.tsx
+++ src/components/MyPage/MyInfo/index.tsx
@@ -9,8 +9,12 @@ export default function MyInfo() {
     <St.MyInfoContainer>
       {userProfile && (
         <St.Profile>
-          <St.ProfileImage src={userProfile.data.profile_image_url} alt="프로필이미지" />
-          <IcChangeProfileBtn />
+          <St.Images>
+            <St.ProfileImage src={userProfile.data.profile_image_url} alt="프로필이미지" />
+            <St.ChangeButton>
+              <IcChangeProfileBtn />
+            </St.ChangeButton>
+          </St.Images>
           <St.ProfileDetail>
             <St.ProfileNickname>
               <St.ProfileMyNickname>{userProfile.data.nickname}</St.ProfileMyNickname>
