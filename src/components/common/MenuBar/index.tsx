fa5167454d6e5496c5f389e7d3be38e2367ab29d
Hennization <shy6295@ewhain.net>
2022-07-18 17:24:59 +0900

    [ feat ] 메뉴바에 card collection 클릭 시 새로고침

--- src/components/common/MenuBar/index.tsx
+++ src/components/common/MenuBar/index.tsx
@@ -17,6 +17,7 @@ export default function MenuBar() {
   const moveCardCollection = () => {
     navigate("/card-collection", { state: { type: "all" } });
     closeMenuBar();
+    location.reload();
   };
 
   const closeMenuBar = () => {
