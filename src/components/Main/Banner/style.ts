7ec94a95a3d9c01292393d4298e3c0daa90027b5
NYeonK <skdus728@gmail.com>
2022-07-18 23:33:49 +0900

    [ design ] banner QA 반영

--- src/components/Main/Banner/style.ts
+++ src/components/Main/Banner/style.ts
@@ -3,37 +3,33 @@ import styled from "styled-components";
 export const St = {
   BannerSlider: styled.section`
     position: relative;
-
     height: 21.7rem;
-
-    //background: ${({ theme }) => theme.colors.green};
   `,
 
   ImageWrapper: styled.img`
     width: 36rem;
     height: 21.7rem;
   `,
-  ContentsPages: styled.span`
+
+  ContentsPages: styled.div`
     position: absolute;
     right: 0;
     bottom: 0;
 
     display: flex;
-    flex-direction: row;
-    align-items: flex-start;
-    padding: 0.4rem 0.7rem 0.5rem;
-    gap: 0.3rem;
+    justify-content: center;
+    gap: 0.8rem;
 
-    width: 3.5rem;
-    height: 1.7rem;
+    width: 3.9rem;
+    height: 1.8rem;
 
     background: rgba(0, 0, 0, 0.5);
   `,
 
   CurrentPage: styled.span`
-    line-height: 81.34%;
-    font-weight: 400;
-    font-size: 1rem;
+    line-height: 1.8rem;
+    letter-spacing: -0.03em;
+    ${({ theme }) => theme.fonts.caption4};
     color: ${({ theme }) => theme.colors.white};
   `,
 };
