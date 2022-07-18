b59b9a18dca969377a4fb9d8371937c66048698b
SNUPI <joohaem@daum.net>
2022-07-19 02:30:35 +0900

    [ feat ] add filter tags state recoil

--- src/core/atom/slider.ts
+++ src/core/atom/slider.ts
@@ -2,9 +2,15 @@ import { atom } from "recoil";
 
 enum StateType {
   SLIDER_IDX = "slideridx",
+  FILTER_TAGS = "filtertags",
 }
 
 export const sliderIdxState = atom({
   key: StateType.SLIDER_IDX,
   default: 0,
+});
+
+export const filterTagsState = atom({
+  key: StateType.FILTER_TAGS,
+  default: [],
 });
