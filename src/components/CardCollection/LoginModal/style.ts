35b7324923fe5bab4c932d6e9f2a3c97884143c6
Hennization <shy6295@ewhain.net>
2022-07-19 02:39:46 +0900

    [ refactor ] loginModal 스타일링 정리

--- src/components/CardCollection/LoginModal/style.ts
+++ src/components/CardCollection/LoginModal/style.ts
@@ -29,8 +29,6 @@ export const St = {
 
   Buttons: styled.div`
     display: flex;
-    flex-direction: row;
-    align-items: flex-start;
     gap: 0.8rem;
 
     width: 29.6rem;
@@ -41,14 +39,12 @@ export const St = {
 
   Button: styled.button`
     display: flex;
-    flex-direction: row;
     justify-content: center;
     align-items: center;
     padding: 0.8rem 4.8rem;
 
     width: 14.4rem;
     height: 3.6rem;
-    background: ${({ theme }) => theme.colors.white};
 
     box-shadow: 0rem 0.1rem 0.1rem rgba(0, 0, 0, 0.25);
     border-radius: 4.7rem;
@@ -56,6 +52,7 @@ export const St = {
     letter-spacing: -0.03rem;
     ${({ theme }) => theme.fonts.btn1};
     color: ${({ theme }) => theme.colors.bg};
+    background: ${({ theme }) => theme.colors.white};
 
     &:active {
       color: ${({ theme }) => theme.colors.white};
