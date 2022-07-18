687b32b55f3e372772437cbbad2f7fc7bb5cf096
JY <pianowell96@gmail.com>
2022-07-18 19:50:54 +0900

    [ refactor ] 로그인 모달창 props 생성

--- src/components/CardCollection/LoginModal/index.tsx
+++ src/components/CardCollection/LoginModal/index.tsx
@@ -4,16 +4,16 @@ import { St } from "./style";
 
 interface LoginCheckProps {
   closeHandler: () => void;
+  contents: string;
 }
-
 export default function LoginModal(props: LoginCheckProps) {
-  const { closeHandler } = props;
+  const { closeHandler, contents } = props;
   return (
     <Modal closeHandler={closeHandler}>
       <St.Container>
         <St.Wrapper>
           <IcGrayHeart />
-          <St.ModalContents>로그인을 하시면 북마크 기능인 마이피클을 이용할 수 있어요!</St.ModalContents>
+          <St.ModalContents>로그인을 하시면 {contents} 이용할 수 있어요!</St.ModalContents>
         </St.Wrapper>
         <St.Buttons>
           <St.LoginButton to="/login">로그인</St.LoginButton>
