2212f9b1e9e3022143a69d3c4813f84761bdcd56
SNUPI <joohaem@daum.net>
2022-07-19 18:26:41 +0900

    [ feat ] 404 page

---
+++ src/components/Error404/style.ts
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const St = {
+  Root: styled.main`
+    height: 100vh;
+
+    padding: 6.4rem 1.6rem 0;
+
+    background-color: ${({ theme }) => theme.colors.sub_green5};
+  `,
+
+  Title: styled.h1`
+    ${({ theme }) => theme.fonts.h1};
+  `,
+
+  Content: styled.p`
+    margin-top: 0.8rem;
+
+    ${({ theme }) => theme.fonts.body6};
+    color: ${({ theme }) => theme.colors.gray600};
+  `,
+
+  GoHomeBtn: styled(Link)`
+    position: absolute;
+    bottom: 6.5rem;
+    left: 50%;
+    transform: translateX(-50%);
+
+    width: 9rem;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-bottom: 0.08rem solid ${({ theme }) => theme.colors.gray600};
+
+    ${({ theme }) => theme.fonts.btn2};
+  `,
+};
