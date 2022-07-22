a5e99a38eaef017f025b684eecf41f2c0afdc476
SNUPI <joohaem@daum.net>
2022-07-22 16:13:47 +0900

    [ feat ] mypage logtou

--- src/components/MyPage/MySetting/index.tsx
+++ src/components/MyPage/MySetting/index.tsx
@@ -1,10 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 import { St } from "./style";
 
 export default function MySetting() {
+  const navigate = useNavigate();
+
+  const handleLogout = (key: string) => {
+    if (key !== "로그아웃") return;
+
+    localStorage.removeItem("piickle-token");
+    navigate("/");
+  };
+
   const mySetting = ["공지사항", "문의", "팀 정보", "로그아웃", "회원 탈퇴"];
   const mySettingList = mySetting.map((setting, index) => (
     <St.MySettingList key={index}>
-      <St.MySettingListDetail>{setting}</St.MySettingListDetail>
+      <St.MySettingListDetail onClick={() => handleLogout(setting)}>{setting}</St.MySettingListDetail>
     </St.MySettingList>
   ));
 
