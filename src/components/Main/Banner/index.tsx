e712c266f791e6011c5dd12042d0c510c250f4f5
SNUPI <joohaem@daum.net>
2022-07-22 17:11:58 +0900

    [ refactor ] 배너 이미지 로직 수정

--- src/components/Main/Banner/index.tsx
+++ src/components/Main/Banner/index.tsx
@@ -11,22 +11,22 @@ type BannerImage = {
   src: string;
   alt: string;
 };
-const BannerImage: BannerImage[] = [
+const bannerImage: BannerImage[] = [
   {
     src: ImgBanner1,
-    alt: "피클 소개 이미지",
+    alt: "지금 내게 필요한 대화주제 추천 서비스 피클 시작하기",
   },
   {
     src: ImgBanner2,
-    alt: "피클 소개 이미지",
+    alt: "헬로우 피클 피플",
   },
   {
     src: ImgBanner3,
-    alt: "피클 소개 이미지",
+    alt: "무드 피클",
   },
   {
     src: ImgBanner4,
-    alt: "피클 소개 이미지",
+    alt: "렛츠 피클",
   },
 ];
 
@@ -47,13 +47,15 @@ export default function Banner() {
   return (
     <St.BannerSlider>
       <Slider {...settings}>
-        {BannerImage.map((img, index) => (
+        {bannerImage.map((img, index) => (
           <St.ImageWrapper key={index} src={img.src} alt={img.alt} />
         ))}
       </Slider>
 
       <St.ContentsPages>
-        <St.CurrentPage>{currentSlide + 1} / 4</St.CurrentPage>
+        <St.CurrentPage>
+          {currentSlide + 1} / {bannerImage.length}
+        </St.CurrentPage>
       </St.ContentsPages>
     </St.BannerSlider>
   );
