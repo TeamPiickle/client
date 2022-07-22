73cda29e1ae0daa3ee91abc9ab92f0f196089541
SNUPI <joohaem@daum.net>
2022-07-22 16:15:57 +0900

    [ refactor ] handling click

--- src/components/MyPage/MySetting/index.tsx
+++ src/components/MyPage/MySetting/index.tsx
@@ -5,17 +5,19 @@ import { St } from "./style";
 export default function MySetting() {
   const navigate = useNavigate();
 
-  const handleLogout = (key: string) => {
-    if (key !== "로그아웃") return;
-
-    localStorage.removeItem("piickle-token");
-    navigate("/");
+  const handleClickDetail = (key: string) => {
+    switch (key) {
+      case "로그아웃":
+        localStorage.removeItem("piickle-token");
+        navigate("/");
+        break;
+    }
   };
 
   const mySetting = ["공지사항", "문의", "팀 정보", "로그아웃", "회원 탈퇴"];
   const mySettingList = mySetting.map((setting, index) => (
     <St.MySettingList key={index}>
-      <St.MySettingListDetail onClick={() => handleLogout(setting)}>{setting}</St.MySettingListDetail>
+      <St.MySettingListDetail onClick={() => handleClickDetail(setting)}>{setting}</St.MySettingListDetail>
     </St.MySettingList>
   ));
 
