7adf4a01b1b27befaedc201af283eaf55f8c849b
SNUPI <joohaem@daum.net>
2022-07-23 02:59:45 +0900

    [ bug ] DEMODAY VERSION

--- src/components/Login/LoginForm/index.tsx
+++ src/components/Login/LoginForm/index.tsx
@@ -13,35 +13,40 @@ export default function LoginForm() {
   const submitLoginForm = async (e: React.FormEvent<HTMLElement>) => {
     e.preventDefault();
 
-    try {
-      const { data } = await loginApi.postLogin(inputRefs.current[0].value, inputRefs.current[1].value);
-      LoginNGoMain(data.accessToken);
-    } catch (error) {
-      if (axios.isAxiosError(error)) {
-        const errorData = error.response?.data;
-        const errorStatus = errorData.status;
+    LoginNGoMain(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjJkNmJiMzEwMWRiNGIyNmRhM2M5NWFlIn0sImlhdCI6MTY1ODM4NDIwMCwiZXhwIjoxNjg5OTIwMjAwfQ.G6vpi0Tr3LmALeUpQuvXXx_KjuROKHWMQ9UAtCMs8mY",
+    );
+    // 위에꺼 지우고 아래꺼 주석 풀면 됨
 
-        switch (errorStatus) {
-          case 200:
-            break;
-          case 400:
-            setErrorMessage({ emailError: "", passwordError: "필요한 값이 없습니다." });
-            break;
-          case 401:
-            if (errorData.message === "존재하지 않는 email 입니다.")
-              setErrorMessage({ passwordError: "", emailError: "존재하지 않는 email 입니다." });
-            else if (errorData.message === "비밀번호가 일치하지 않습니다.")
-              setErrorMessage({ emailError: "", passwordError: "비밀번호가 일치하지 않습니다." });
-            break;
-          case 500:
-            // 서버 내부 오류
-            navigate("/login");
-            break;
-          default:
-            throw new Error("로그인 에러");
-        }
-      }
-    }
+    // try {
+    //   const { data } = await loginApi.postLogin(inputRefs.current[0].value, inputRefs.current[1].value);
+    //   LoginNGoMain(data.accessToken);
+    // } catch (error) {
+    //   if (axios.isAxiosError(error)) {
+    //     const errorData = error.response?.data;
+    //     const errorStatus = errorData.status;
+
+    //     switch (errorStatus) {
+    //       case 200:
+    //         break;
+    //       case 400:
+    //         setErrorMessage({ emailError: "", passwordError: "필요한 값이 없습니다." });
+    //         break;
+    //       case 401:
+    //         if (errorData.message === "존재하지 않는 email 입니다.")
+    //           setErrorMessage({ passwordError: "", emailError: "존재하지 않는 email 입니다." });
+    //         else if (errorData.message === "비밀번호가 일치하지 않습니다.")
+    //           setErrorMessage({ emailError: "", passwordError: "비밀번호가 일치하지 않습니다." });
+    //         break;
+    //       case 500:
+    //         // 서버 내부 오류
+    //         navigate("/login");
+    //         break;
+    //       default:
+    //         throw new Error("로그인 에러");
+    //     }
+    //   }
+    // }
   };
 
   const LoginNGoMain = (accessToken: string) => {
