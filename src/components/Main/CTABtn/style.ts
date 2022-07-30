b1da1ed593cc73d6af8551abb2e8eb865ac2c440
SNUPI <joohaem@daum.net>
2022-07-31 08:54:01 +0900

    [ refactor ] apply media theme

--- src/components/Main/CTABtn/style.ts
+++ src/components/Main/CTABtn/style.ts
@@ -16,13 +16,14 @@ export const St = {
     background-color: ${({ theme }) => theme.colors.black};
 
     width: calc(100% - 3.2rem);
-    @media screen and (min-width: 48rem), screen and (min-height: 48rem) and (orientation: landscape) {
-      width: 32.8rem;
-    }
     height: 5rem;
 
     backdrop-filter: blur(1.2rem);
     color: ${({ theme }) => theme.colors.white};
     ${({ theme }) => theme.fonts.btn2};
+
+    ${({ theme }) => theme.media.desktop`
+      width: 32.8rem;
+    `};
   `,
 };
