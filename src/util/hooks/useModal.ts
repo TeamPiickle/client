493c5fcca1e5201627a0291fdc83e647c1b60ab9
SNUPI <joohaem@daum.net>
2022-08-01 17:34:22 +0900

    [ refactor ] useModal on MyInfo

--- src/util/hooks/useModal.ts
+++ src/util/hooks/useModal.ts
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+
+import { activeStateModal } from "../../core/atom/menuBar";
 
 export default function useModal() {
+  const setIsActiveModal = useSetRecoilState(activeStateModal);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const toggleModal = () => {
-    setIsModalOpen((prevModalState) => !prevModalState);
+    setIsModalOpen((prevModalState) => {
+      setIsActiveModal(!prevModalState);
+      return !prevModalState;
+    });
   };
 
   return { isModalOpen, toggleModal };
