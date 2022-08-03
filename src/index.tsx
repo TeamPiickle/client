160696da84477ec27858377bec0ce58f3712cbe9
SNUPI <joohaem@daum.net>
2022-08-03 17:42:08 +0900

    [ bug ] add serviceWorker with react-ts

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,12 @@ import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 
 import App from "./App";
+// import registerServiceWorker from "./serviceWorker";
 import { GlobalStyle } from "./style/globalStyle";
 import theme from "./style/theme";
+
+// service worker for PWA
+// registerServiceWorker();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
