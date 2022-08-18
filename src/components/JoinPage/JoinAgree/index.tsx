0b8a2cb3d5dae2d28a3415ba29d1ba58b5d8df3f
NYeonK <skdus728@gmail.com>
2022-08-18 19:34:52 +0900

    [ feat ] JoinAgree 레이아웃 구현

--- src/components/JoinPage/JoinAgree/index.tsx
+++ src/components/JoinPage/JoinAgree/index.tsx
@@ -1,5 +1,34 @@
+import React, { useState } from "react";
+
 import { St } from "./style";
 
 export default function JoinAgree() {
-  return <St.JoinAgree>회원가입페이지</St.JoinAgree>;
+  const [isPicked, setIsPicked] = useState({
+    first: false,
+    second: false,
+    third: false,
+  });
+
+  const [allPicked, setAllPicked] = useState(false);
+
+  return (
+    <St.JoinAgree>
+      <St.AgreeTitle>약관을 동의해주세요</St.AgreeTitle>
+      <St.AgreeContent>
+        <St.AllAgree>약관 전체동의</St.AllAgree>
+        <br></br>
+        <St.AgreeListFisrt>
+          <St.AgreeListContent>이용약관 동의 (필수)</St.AgreeListContent>
+          <button></button>
+        </St.AgreeListFisrt>
+        <St.AgreeListSecond>
+          <St.AgreeListContent>개인정보 수집 및 이용동의 (필수)</St.AgreeListContent>
+          <button></button>
+        </St.AgreeListSecond>
+        <St.AgreeListThird>
+          <St.AgreeListContent>마케팅 활용/광고성 정부 수신동의 (선택)</St.AgreeListContent>
+        </St.AgreeListThird>
+      </St.AgreeContent>
+    </St.JoinAgree>
+  );
 }
--- src/components/JoinPage/JoinAgree/style.ts
+++ src/components/JoinPage/JoinAgree/style.ts
@@ -2,4 +2,16 @@ import styled from "styled-components";
 
 export const St = {
   JoinAgree: styled.section``,
+
+  AgreeTitle: styled.title``,
+
+  AgreeContent: styled.ul``,
+
+  AllAgree: styled.strong``,
+
+  AgreeListFisrt: styled.li``,
+  AgreeListSecond: styled.li``,
+  AgreeListThird: styled.li``,
+
+  AgreeListContent: styled.strong``,
 };
