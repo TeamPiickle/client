985959fc3f2771615edef8e0abdb2594956b0f67
NYeonK <skdus728@gmail.com>
2022-08-18 18:22:42 +0900

    [ feat ] JoinPage 폴더 및 JoinAgree 컴포넌트 생성

---
+++ src/components/JoinPage/JoinAgree/index.tsx
@@ -0,0 +1,5 @@
+import { St } from "./style";
+
+export default function JoinAgree() {
+  return <St.JoinAgree>회원가입페이지</St.JoinAgree>;
+}
---
+++ src/components/JoinPage/index.tsx
@@ -0,0 +1,11 @@
+import Footer from "../common/Footer";
+import JoinAgree from "./JoinAgree";
+
+export default function JoinPage() {
+  return (
+    <main>
+      <JoinAgree />
+      <Footer />
+    </main>
+  );
+}
