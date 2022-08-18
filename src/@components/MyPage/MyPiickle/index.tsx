b1d264e380eb234b042cccc11063dbb57ed30953
SNUPI <joohaem@daum.net>
2022-08-18 21:37:12 +0900

    [ etc ] @폴더구조 순서 재조정

---
+++ src/@components/MyPage/MyPiickle/index.tsx
@@ -0,0 +1,21 @@
+import { IcFullHeart, IcRightArrow } from "../../../asset/icon/index";
+import { routePaths } from "../../../core/routes/path";
+import { St } from "./style";
+
+export default function MyPiickle() {
+  return (
+    <St.MyPiickleContainer>
+      <St.MyPiickleWrapper>
+        <IcFullHeart />
+        <St.TitleWrapper>
+          <St.Title>My Piickle</St.Title>
+          <St.SubTitle>내가 저장한 피클들</St.SubTitle>
+        </St.TitleWrapper>
+      </St.MyPiickleWrapper>
+      <St.MoveButton to={routePaths.BookmarkPage}>
+        <St.MoveButtonTitle>카드 모아보기</St.MoveButtonTitle>
+        <IcRightArrow />
+      </St.MoveButton>
+    </St.MyPiickleContainer>
+  );
+}
