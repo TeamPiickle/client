b5b7a9e00eeac5010125e3dcb7d4643ab5bf91ad
NYeonK <skdus728@gmail.com>
2022-08-20 19:00:44 +0900

    [ design ] JoinAgree page css 완성

--- src/components/JoinPage/JoinAgree/style.ts
+++ src/components/JoinPage/JoinAgree/style.ts
@@ -1,19 +1,74 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const St = {
-  JoinAgree: styled.section``,
+  JoinAgree: styled.section`
+    height: 54.2rem;
+  `,
 
-  AgreeTitle: styled.article``,
+  AgreeTitle: styled.article`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    padding: 2.4rem 1.6rem 3.2rem;
+    height: 9.2rem;
+
+    ${({ theme }) => theme.fonts.h1};
+    color: ${({ theme }) => theme.colors.bg};
+  `,
 
   AgreeContent: styled.ul``,
 
-  AgreeContentItem: styled.li``,
-};
+  AgreeContentItem: styled.li`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    flex-wrap: wrap;
 
-/*
-  AllAgree: styled.strong``,
-  AgreeListFisrt: styled.li``,
-  AgreeListSecond: styled.li``,
-  AgreeListThird: styled.li``,
-  AgreeListContent: styled.strong``,
-*/
+    gap: 1.6rem;
+    padding: 1.6rem;
+    height: 5.6rem;
+
+    ${({ theme }) => theme.fonts.body12};
+    color: ${({ theme }) => theme.colors.bg};
+  `,
+
+  DetailBtn: styled(Link)`
+    position: absolute;
+    right: 0.8rem;
+  `,
+
+  Line: styled.div`
+    position: absolute;
+    margin-top: 5.6rem;
+    margin-left: -1.6rem;
+    width: 36rem;
+
+    hr {
+      height: 0.2rem;
+      border-width: 0;
+      background-color: ${({ theme }) => theme.colors.gray100};
+      color: ${({ theme }) => theme.colors.gray100};
+    }
+  `,
+
+  Button: styled.button`
+    margin: auto;
+    display: block;
+    margin-top: 15.4rem;
+
+    border-radius: 5rem;
+    background-color: ${({ theme }) => theme.colors.bg};
+
+    width: calc(100% - 3.2rem);
+    height: 5rem;
+
+    color: ${({ theme }) => theme.colors.white};
+    ${({ theme }) => theme.fonts.btn2};
+
+    ${({ theme }) => theme.media.desktop`
+      width: 32.8rem;
+    `};
+  `,
+};
