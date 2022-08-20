c1a75e11519c3ba05d9c261e9a7a6b44c23a301e
NYeonK <skdus728@gmail.com>
2022-08-21 00:17:31 +0900

    [ feat ] 클릭 시, checkbox 이미지 변경

--- src/components/JoinPage/JoinAgree/index.tsx
+++ src/components/JoinPage/JoinAgree/index.tsx
@@ -1,27 +1,55 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { IcEmptyCheckBox, IcFullCheckBox, IcNextBtn } from "../../../asset/icon";
 import { St } from "./style";
 
 export default function JoinAgree() {
-  const [isPicked, setIsPicked] = useState({
-    first: false,
-    second: false,
-    third: false,
-  });
+  const [isPicked, setIsPicked] = useState(0);
 
   const [allPicked, setAllPicked] = useState(false);
-
   const [lists, setLists] = useState([
-    { id: 1, checkBox: <IcEmptyCheckBox />, text: "약관 전체동의", line: <hr /> },
-    { id: 2, checkBox: <IcEmptyCheckBox />, text: "이용약관 동의 (필수)", button: <IcNextBtn /> },
-    { id: 3, checkBox: <IcEmptyCheckBox />, text: "개인정보 수집 및 이용동의 (필수)", button: <IcNextBtn /> },
-    { id: 4, checkBox: <IcEmptyCheckBox />, text: "마케팅 활용/광고성 정부 수신동의 (선택)" },
+    { id: 1, state: false, checkBox: <IcEmptyCheckBox />, text: "약관 전체동의", line: <hr /> },
+    { id: 2, state: false, checkBox: <IcEmptyCheckBox />, text: "이용약관 동의 (필수)", button: <IcNextBtn /> },
+    {
+      id: 3,
+      state: false,
+      checkBox: <IcEmptyCheckBox />,
+      text: "개인정보 수집 및 이용동의 (필수)",
+      button: <IcNextBtn />,
+    },
+    { id: 4, state: false, checkBox: <IcEmptyCheckBox />, text: "마케팅 활용/광고성 정부 수신동의 (선택)" },
   ]);
 
-  const agreeList = lists.map((item) => (
+  function ChangeCheckBox(index: number) {
+    const currentList = [...lists];
+    const isPicked = index;
+    setIsPicked(index);
+    console.log("오류값", isPicked);
+    if (currentList[index].state === false) {
+      currentList[index].checkBox = <IcFullCheckBox />;
+    } else if (currentList[index].state === true) {
+      currentList[index].checkBox = <IcEmptyCheckBox />;
+    }
+    setLists(currentList);
+  }
+
+  useEffect(() => {
+    console.log(lists);
+    console.log("lists이라는 값이 업데이트 될 때만 실행");
+
+    if (lists[isPicked].state === false) {
+      lists[isPicked].state = true;
+    } else if (lists[isPicked].state === true) {
+      lists[isPicked].state = false;
+    }
+  }, [isPicked, lists]);
+
+  const agreeList = lists.map((item, index) => (
     <St.AgreeContentItem key={item.id}>
-      {item.checkBox} {item.text}
+      <button type="button" onClick={() => ChangeCheckBox(index)}>
+        {item.checkBox}
+      </button>
+      {item.text}
       <St.DetailBtn to="">{item.button}</St.DetailBtn>
       <St.Line>{item.line}</St.Line>
     </St.AgreeContentItem>
