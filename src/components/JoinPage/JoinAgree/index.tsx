6defbb7af5847b092892e5de208ea5b8b1d6d6f4
NYeonK <skdus728@gmail.com>
2022-08-23 06:26:08 +0900

    [ feat ] 필수 조건을 다 누르면 경고창 숨김

--- src/components/JoinPage/JoinAgree/index.tsx
+++ src/components/JoinPage/JoinAgree/index.tsx
@@ -5,20 +5,12 @@ import { St } from "./style";
 
 export default function JoinAgree() {
   const [isPicked, setIsPicked] = useState(0);
-  //const [allPicked, setAllPicked] = useState(false);
+  const [allPicked, setAllPicked] = useState(false);
   const [activeBtn, setActiveBtn] = useState(0);
 
-  const [openAlert, setOpenAlert] = useState(false);
+  const [checkItems, setCheckItems] = useState([0]);
 
-  function alertClassName() {
-    if (openAlert === false) {
-      return "login-alert";
-    } else {
-      // 경고창 띄우기
-      // setOpenAlert(true);
-      return "login-alert-view";
-    }
-  }
+  const [openAlert, setOpenAlert] = useState(false);
 
   const [lists, setLists] = useState([
     { id: 1, state: false, checkBox: <IcEmptyCheckBox />, text: "약관 전체동의", line: <hr /> },
@@ -39,7 +31,6 @@ export default function JoinAgree() {
     },
     { id: 5, state: false, checkBox: <IcEmptyCheckBox />, text: "만 14세 이상입니다 (필수)" },
   ]);
-
   function ChangeCheckBox(index: number) {
     const currentList = [...lists];
     setIsPicked(index);
@@ -49,56 +40,79 @@ export default function JoinAgree() {
     } else if (currentList[index].state === true) {
       currentList[index].checkBox = <IcEmptyCheckBox />;
     }
-    // aLLClickCheckBox();
+
+    handleAllCheck(currentList[index].state);
+
+    if (checkItems.length === 5) {
+      console.log("반영확인1");
+      CheckAllPicked();
+    } else {
+      console.log("반영확인2");
+      currentList[0].checkBox = <IcFullCheckBox />;
+      CheckAllPicked();
+    }
+    if (currentList[index].id === 1) {
+      console.log(currentList[index].state);
+      console.log(checkItems);
+
+      // 데이터 개수와 체크된 아이템의 개수가 다를 경우 선택 해제 (하나라도 해제 시 선택 해제)
+    }
     setLists(currentList);
   }
 
   useEffect(() => {
     console.log(lists);
-
     if (lists[isPicked].state === false) {
       lists[isPicked].state = true;
-      //lists[isPicked].checkBox = <IcFullCheckBox />;
     } else if (lists[isPicked].state === true) {
       lists[isPicked].state = false;
-      //lists[isPicked].checkBox = <IcEmptyCheckBox />;
     }
-    // button 활성화
-    /*
-    if (lists[1].state === true) {
-      setActiveBtn(activeBtn + 1);
-      console.log(activeBtn);
-    } else if (lists[2].state === true) {
-      setActiveBtn(activeBtn + 1);
-      console.log(activeBtn);
-    } else if (lists[1].state === false) {
-      setActiveBtn(activeBtn - 1);
-      console.log(activeBtn);
-    } else if (lists[2].state === false) {
-      setActiveBtn(activeBtn - 1);
-      console.log(activeBtn);
-    } else {
-      setActiveBtn(activeBtn + 0);
-      console.log(activeBtn);
-    }
-    */
   }, [isPicked, lists]);
 
-  //  const aLLClickCheckBox = () => {
-  //    const allCurrentList = [...lists];
-  //
-  //    if (allCurrentList[0].state === true) {
-  //      console.log("모두 비동의");
-  //      allCurrentList[1].state = false;
-  //      allCurrentList[2].state = false;
-  //      allCurrentList[3].state = false;
-  //    } else {
-  //      console.log("모두 동의");
-  //      allCurrentList[1].state = true;
-  //      allCurrentList[2].state = true;
-  //      allCurrentList[3].state = true;
-  //    }
-  //  };
+  function CheckAllPicked() {
+    const currentList = [...lists];
+
+    if (allPicked === false) {
+      currentList[0].checkBox = <IcFullCheckBox />;
+      currentList[1].checkBox = <IcFullCheckBox />;
+      currentList[2].checkBox = <IcFullCheckBox />;
+      currentList[3].checkBox = <IcFullCheckBox />;
+      currentList[4].checkBox = <IcFullCheckBox />;
+      //currentList[0].state = true;
+      currentList[1].state = true;
+      currentList[2].state = true;
+      currentList[3].state = true;
+      currentList[4].state = true;
+    } else {
+      currentList[0].checkBox = <IcEmptyCheckBox />;
+      currentList[1].checkBox = <IcEmptyCheckBox />;
+      currentList[2].checkBox = <IcEmptyCheckBox />;
+      currentList[3].checkBox = <IcEmptyCheckBox />;
+      currentList[4].checkBox = <IcEmptyCheckBox />;
+      //currentList[0].state = false;
+      currentList[1].state = false;
+      currentList[2].state = false;
+      currentList[3].state = false;
+      currentList[4].state = false;
+    }
+  }
+  /* 계속 하던거
+  function checkAllStatus() {
+    //const { termsOfService, privacyPolicy, allowPromotions } = agreements;
+    //lists[1].state === true && lists[2].state === true && lists[3].state === true
+    if (lists[1].state && lists[2].state && lists[3].state && lists[4].state) {
+      setAllPicked(false);
+      CheckAllPicked();
+
+      console.log("모두 동의");
+    } else {
+      setAllPicked(true);
+      CheckAllPicked();
+
+      console.log("모두 비동의");
+    }
+  }
+*/
   /*
   useEffect(() => {
     if (lists[1].state === true && lists[2].state === true && lists[3].state === true) {
@@ -136,6 +150,47 @@ export default function JoinAgree() {
   //     setAllPicked(false);
   //   }
   // }
+
+  // 체크박스 전체 선택
+  const handleAllCheck = (checked: boolean) => {
+    if (checked) {
+      // 전체 선택 클릭 시 데이터의 모든 아이템(id)를 담은 배열로 checkItems 상태 업데이트
+      const idArray: Array<number> = [];
+      lists.forEach((el) => idArray.push(el.id));
+      setCheckItems(idArray);
+    } else {
+      // 전체 선택 해제 시 checkItems 를 빈 배열로 상태 업데이트
+      setCheckItems([]);
+    }
+  };
+
+  function alertClassName() {
+    const currentList = [...lists];
+    let alert = false;
+    if (currentList[0].state === false) {
+      //setOpenAlert(false);
+      alert = false;
+    } else if (currentList[1].state === false && currentList[2].state === false && currentList[4].state === false) {
+      //setOpenAlert(false);
+      alert = false;
+    } else {
+      //setOpenAlert(true);
+      alert = true;
+    }
+    if (alert === false) {
+      return "login-alert";
+    } else {
+      // 경고창 띄우기
+      // setOpenAlert(true);
+      return "login-alert-view";
+    }
+    // return openAlert === false ? "login-alert" : "login-alert-view";
+  }
+
+  useEffect(() => {
+    console.log("좀..");
+  }, [setOpenAlert]);
+
   const agreeList = lists.map((item, index) => (
     <St.AgreeContentItem key={item.id}>
       <St.CheckBox type="button" onClick={() => ChangeCheckBox(index)}>
