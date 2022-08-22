a24d3a07f7841d84290c05df4d4c93a0fd16ef21
NYeonK <skdus728@gmail.com>
2022-08-22 23:50:27 +0900

    [ feat ] List 요소 추가

--- src/components/JoinPage/JoinAgree/index.tsx
+++ src/components/JoinPage/JoinAgree/index.tsx
@@ -5,8 +5,9 @@ import { St } from "./style";
 
 export default function JoinAgree() {
   const [isPicked, setIsPicked] = useState(0);
+  //const [allPicked, setAllPicked] = useState(false);
+  const [activeBtn, setActiveBtn] = useState(0);
 
-  const [allPicked, setAllPicked] = useState(false);
   const [lists, setLists] = useState([
     { id: 1, state: false, checkBox: <IcEmptyCheckBox />, text: "약관 전체동의", line: <hr /> },
     { id: 2, state: false, checkBox: <IcEmptyCheckBox />, text: "이용약관 동의 (필수)", button: <IcNextBtn /> },
@@ -17,7 +18,14 @@ export default function JoinAgree() {
       text: "개인정보 수집 및 이용동의 (필수)",
       button: <IcNextBtn />,
     },
-    { id: 4, state: false, checkBox: <IcEmptyCheckBox />, text: "마케팅 활용/광고성 정부 수신동의 (선택)" },
+    {
+      id: 4,
+      state: false,
+      checkBox: <IcEmptyCheckBox />,
+      text: "마케팅 활용/광고성 정부 수신동의 (선택)",
+      button: <IcNextBtn />,
+    },
+    { id: 5, state: false, checkBox: <IcEmptyCheckBox />, text: "만 14세 이상입니다 (필수)" },
   ]);
 
   function ChangeCheckBox(index: number) {
@@ -29,6 +37,8 @@ export default function JoinAgree() {
     } else if (currentList[index].state === true) {
       currentList[index].checkBox = <IcEmptyCheckBox />;
     }
+    // aLLClickCheckBox();
+
     setLists(currentList);
   }
 
@@ -37,18 +47,91 @@ export default function JoinAgree() {
 
     if (lists[isPicked].state === false) {
       lists[isPicked].state = true;
+      //lists[isPicked].checkBox = <IcFullCheckBox />;
     } else if (lists[isPicked].state === true) {
       lists[isPicked].state = false;
+      //lists[isPicked].checkBox = <IcEmptyCheckBox />;
     }
+    // button 활성화
+    /*
+    if (lists[1].state === true) {
+      setActiveBtn(activeBtn + 1);
+      console.log(activeBtn);
+    } else if (lists[2].state === true) {
+      setActiveBtn(activeBtn + 1);
+      console.log(activeBtn);
+    } else if (lists[1].state === false) {
+      setActiveBtn(activeBtn - 1);
+      console.log(activeBtn);
+    } else if (lists[2].state === false) {
+      setActiveBtn(activeBtn - 1);
+      console.log(activeBtn);
+    } else {
+      setActiveBtn(activeBtn + 0);
+      console.log(activeBtn);
+    }
+    */
   }, [isPicked, lists]);
 
+  //  const aLLClickCheckBox = () => {
+  //    const allCurrentList = [...lists];
+  //
+  //    if (allCurrentList[0].state === true) {
+  //      console.log("모두 비동의");
+  //      allCurrentList[1].state = false;
+  //      allCurrentList[2].state = false;
+  //      allCurrentList[3].state = false;
+  //    } else {
+  //      console.log("모두 동의");
+  //      allCurrentList[1].state = true;
+  //      allCurrentList[2].state = true;
+  //      allCurrentList[3].state = true;
+  //    }
+  //  };
+  /*
+  useEffect(() => {
+    if (lists[1].state === true && lists[2].state === true && lists[3].state === true) {
+      setAllPicked(true);
+      lists[0].state === true;
+    } else if (lists[0].state === true) {
+      setAllPicked(true);
+      lists[0].state === true;
+    } else {
+      setAllPicked(false);
+      lists[0].state === false;
+    }
+  }, [lists]);
+*/
+  // function aLLClickCheckBox1() {
+  //   const currentList = [...lists];
+  //   console.log(currentList[1].state && currentList[2].state && currentList[3].state);
+  //   if (currentList[1].state === true && currentList[2].state === true && currentList[3].state === true) {
+  //     currentList[0].checkBox = <IcFullCheckBox />;
+  //     currentList[0].state === true;
+  //     setAllPicked(true);
+  //   } else if (currentList[0].state === true) {
+  //     setAllPicked(true);
+  //     currentList[1].checkBox = <IcFullCheckBox />;
+  //     currentList[2].checkBox = <IcFullCheckBox />;
+  //     currentList[3].checkBox = <IcFullCheckBox />;
+  //     currentList[1].state === true && currentList[2].state === true && currentList[3].state === true;
+  //   } else if (currentList[0].state === false) {
+  //     setAllPicked(false);
+  //     currentList[1].checkBox = <IcEmptyCheckBox />;
+  //     currentList[2].checkBox = <IcEmptyCheckBox />;
+  //     currentList[3].checkBox = <IcEmptyCheckBox />;
+  //     currentList[1].state === false && currentList[2].state === false && currentList[3].state === false;
+  //   } else if ((currentList[0].checkBox = <IcEmptyCheckBox />)) {
+  //     setAllPicked(false);
+  //   }
+  // }
   const agreeList = lists.map((item, index) => (
     <St.AgreeContentItem key={item.id}>
-      <button type="button" onClick={() => ChangeCheckBox(index)}>
+      <St.CheckBox type="button" onClick={() => ChangeCheckBox(index)}>
         {item.checkBox}
-      </button>
+      </St.CheckBox>
       {item.text}
-      <St.DetailBtn to="">{item.button}</St.DetailBtn>
+      <St.DetailButton to="">{item.button}</St.DetailButton>
       <St.Line>{item.line}</St.Line>
     </St.AgreeContentItem>
   ));
@@ -57,7 +140,9 @@ export default function JoinAgree() {
     <St.JoinAgree>
       <St.AgreeTitle>약관을 동의해주세요</St.AgreeTitle>
       <St.AgreeContent>{agreeList}</St.AgreeContent>
-      <St.Button type="button">회원가입 완료하기</St.Button>
+      <St.JoinButton onClick={() => (location.href = "")}>회원가입 완료하기</St.JoinButton>
     </St.JoinAgree>
   );
 }
+
+// className={`${activeBtn >= 2 ? "active" : ""}`}
