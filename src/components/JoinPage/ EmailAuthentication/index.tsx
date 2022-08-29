f35d07de85703937780a2004f41efc1a8bf19fd0
JY <pianowell96@gmail.com>
2022-08-30 02:41:57 +0900

    [ refactor ] uncontrolled Input 사용해서 이메일 유효성 검증

--- src/components/JoinPage/ EmailAuthentication/index.tsx
+++ src/components/JoinPage/ EmailAuthentication/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { prevPages } from "../../../core/join/prevPages";
@@ -15,12 +15,19 @@ export default function EmailAuthentication() {
   const [isEmailInValid, setIsEmailInValid] = useState<any>(false);
   const emailInput = useRef<HTMLInputElement | null>(null);
 
-  const onChangeEmail = () => {
-    emailInput.current && setEmailText(emailInput.current.value);
+  const clickSendBtn = () => {
+    if (emailInput.current && emailInput.current.value) {
+      setEmailText(emailInput.current?.value);
+    } else {
+      setIsEmailInValid(true);
+    }
   };
 
-  const verifyEmail = () => {
-    const email: string | null = emailInput.current && emailInput.current.value;
+  useEffect(() => {
+    emailText && verifyEmail(emailText);
+  }, [emailText]);
+
+  const verifyEmail = (email: string) => {
     const regEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
     if (!email || regEmail.test(email) === false) {
@@ -48,14 +55,8 @@ export default function EmailAuthentication() {
             {isEmailInValid ? <St.EssentialIcon>*</St.EssentialIcon> : <St.EssentialText>(필수)</St.EssentialText>}
           </St.DescriptionContainer>
           <St.InputContainer>
-            <St.EmailInputForm
-              id="email"
-              placeholder="hello@piickle.com"
-              ref={emailInput}
-              onChange={onChangeEmail}
-              value={emailText}
-            />
-            <St.SendBtn onClick={verifyEmail}>인증메일 전송</St.SendBtn>
+            <St.EmailInputForm id="email" placeholder="hello@piickle.com" ref={emailInput} />
+            <St.SendBtn onClick={clickSendBtn}>인증메일 전송</St.SendBtn>
           </St.InputContainer>
         </St.EmailAuthenticationContent>
         {isEmailInValid && <St.WarningText>이메일 형식이 올바르지 않습니다</St.WarningText>}
