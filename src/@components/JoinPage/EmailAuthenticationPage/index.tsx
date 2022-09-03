1ac811e0fef0726def4171f949cda3297f46bbe5
SNUPI <joohaem@daum.net>
2022-09-03 15:32:55 +0900

    [ etc ] 변수명 변경

--- src/@components/JoinPage/EmailAuthenticationPage/index.tsx
+++ src/@components/JoinPage/EmailAuthenticationPage/index.tsx
@@ -12,14 +12,14 @@ import { St } from "./style";
 export default function EmailAuthentication() {
   const navigate = useNavigate();
   const [emailText, setEmailText] = useState<string>("");
-  const [isEmailInValid, setIsEmailInValid] = useState<any>(false);
+  const [isEmailInvalid, setIsEmailInvalid] = useState<any>(false);
   const emailInput = useRef<HTMLInputElement | null>(null);
 
   const clickSendBtn = () => {
     if (emailInput.current && emailInput.current.value) {
       setEmailText(emailInput.current?.value);
     } else {
-      setIsEmailInValid(true);
+      setIsEmailInvalid(true);
     }
   };
 
@@ -31,7 +31,7 @@ export default function EmailAuthentication() {
     const regEmail = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
     if (!email || regEmail.test(email) === false) {
-      setIsEmailInValid(true);
+      setIsEmailInvalid(true);
     } else {
       navigate(`${routePaths.Join_}${routePaths.Join_EmailConfirm}`, {
         state: {
@@ -52,14 +52,14 @@ export default function EmailAuthentication() {
         <St.EmailAuthenticationContent>
           <St.DescriptionContainer>
             <St.ContentDescription>이메일</St.ContentDescription>
-            {isEmailInValid ? <St.EssentialIcon>*</St.EssentialIcon> : <St.EssentialText>(필수)</St.EssentialText>}
+            {isEmailInvalid ? <St.EssentialIcon>*</St.EssentialIcon> : <St.EssentialText>(필수)</St.EssentialText>}
           </St.DescriptionContainer>
           <St.InputContainer>
-            <St.EmailInputForm id="email" placeholder="hello@piickle.com" ref={emailInput} />
+            <St.EmailInput id="email" placeholder="hello@piickle.com" ref={emailInput} />
             <St.SendBtn onClick={clickSendBtn}>인증메일 전송</St.SendBtn>
           </St.InputContainer>
         </St.EmailAuthenticationContent>
-        {isEmailInValid && <St.WarningText>이메일 형식이 올바르지 않습니다</St.WarningText>}
+        {isEmailInvalid && <St.WarningText>이메일 형식이 올바르지 않습니다</St.WarningText>}
       </St.EmailAuthenticationSection>
       <Footer />
     </St.Root>
