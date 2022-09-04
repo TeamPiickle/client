452ffa7a6bfe5f84ce0add9dcc3c74ef9897a8ab
NYeonK <skdus728@gmail.com>
2022-09-04 21:47:21 +0900

    [ feat ] UserProfilePage 경로 추가

--- src/@components/index.ts
+++ src/@components/index.ts
@@ -5,6 +5,7 @@ export { default as Error404Page } from "./Error404Page";
 export { default as AgreePage } from "./JoinPage/AgreePage";
 export { default as EmailAuthenticationPage } from "./JoinPage/EmailAuthenticationPage";
 export { default as EmailConfirmPage } from "./JoinPage/EmailConfirmPage";
+export { default as UserProfilePage } from "./JoinPage/UserProfilePage";
 export { default as LoginPage } from "./LoginPage";
 export { default as MainPage } from "./MainPage";
 export { default as MyPage } from "./MyPage";
