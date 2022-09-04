da6ded84839e07f9b1632e5b6b4ebddd08ed39ea
NYeonK <skdus728@gmail.com>
2022-09-04 21:55:49 +0900

    [ feat ] Footer 추가

--- src/@components/JoinPage/UserProfilePage/index.tsx
+++ src/@components/JoinPage/UserProfilePage/index.tsx
@@ -1,5 +1,6 @@
 import { prevPages } from "../../../core/join/prevPages";
 import { progressRate } from "../../../core/join/progressRate";
+import Footer from "../../@common/Footer";
 import Header from "../common/Header";
 import PageProgressBar from "../common/PageProgressBar";
 import { St } from "./style";
@@ -9,6 +10,7 @@ export default function UserProfilePage() {
     <St.Root>
       <Header prevPage={prevPages[4].prevPage} />
       <PageProgressBar rate={progressRate[3].rate} />
+      <Footer />
     </St.Root>
   );
 }
