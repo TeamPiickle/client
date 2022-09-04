d5284c72918cf360fc31cd8da248262f36b937cb
NYeonK <skdus728@gmail.com>
2022-09-04 21:54:51 +0900

    [ feat ] Progress Bar 추가

--- src/@components/JoinPage/UserProfilePage/index.tsx
+++ src/@components/JoinPage/UserProfilePage/index.tsx
@@ -1,11 +1,14 @@
 import { prevPages } from "../../../core/join/prevPages";
+import { progressRate } from "../../../core/join/progressRate";
 import Header from "../common/Header";
+import PageProgressBar from "../common/PageProgressBar";
 import { St } from "./style";
 
 export default function UserProfilePage() {
   return (
     <St.Root>
       <Header prevPage={prevPages[4].prevPage} />
+      <PageProgressBar rate={progressRate[3].rate} />
     </St.Root>
   );
 }
