87cb54ba5db0574dbfc719cebe7fa98979d8c5ab
NYeonK <skdus728@gmail.com>
2022-09-10 05:00:55 +0900

    [ feat ] react-mobile-datepicker를 사용하여 생년월일 설정 기능 구현

--- src/@components/JoinPage/UserProfilePage/ProfileBirth/index.tsx
+++ src/@components/JoinPage/UserProfilePage/ProfileBirth/index.tsx
@@ -1,26 +1,61 @@
+import { useState } from "react";
+import DatePicker from "react-mobile-datepicker";
+
 import { IcDownArrow } from "../../../../asset/icon";
 import { St } from "./style";
 
 export default function ProfileBirth() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isbirth, setIsbirth] = useState<string>("");
+  const [time, setTime] = useState(new Date());
+
+  const dateFormat = (defaultTime: typeof time) => {
+    const year = defaultTime.getFullYear();
+    const month = defaultTime.getMonth() + 1;
+    const day = defaultTime.getDate();
+    const monthResult = month < 10 ? "0" + month : month;
+    const dayResult = day < 10 ? "0" + day : day;
+    return year + "년 " + monthResult + "월 " + dayResult + "일 ";
+  };
+
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsOpen(false);
+  };
+
+  const handleSelect = (nextTime: typeof time) => {
+    const getTime = dateFormat(nextTime);
+    setIsbirth(getTime);
+    setIsOpen(false);
+  };
+
   return (
     <St.ProfileBirth>
-      <St.InputContainer>
-        <St.YearInputForm id="year" />
+      <St.InputContainer onClick={handleClick}>
+        <St.BirthInputForm type="text" placeholder="생년월일을 선택해주세요" value={isbirth} readOnly />
         <St.Down>
           <IcDownArrow />
         </St.Down>
-        <St.Text>년</St.Text>
-        <St.MonthInputForm id="month" />
-        <St.Down>
-          <IcDownArrow />
-        </St.Down>
-        <St.Text>월</St.Text>
-        <St.DateInputForm id="date" />
-        <St.Down>
-          <IcDownArrow />
-        </St.Down>
-        <St.Text>일</St.Text>
       </St.InputContainer>
+      {isOpen && (
+        <St.PickerContainer>
+          <DatePicker
+            value={time}
+            isOpen={isOpen}
+            onSelect={handleSelect}
+            onCancel={handleCancel}
+            isPopup={false}
+            showHeader={false}
+            dateFormat={["YYYY", "MM", "DD"]}
+            theme="ios"
+            confirmText="OK"
+            cancelText="Cancle"
+          />
+        </St.PickerContainer>
+      )}
     </St.ProfileBirth>
   );
 }
