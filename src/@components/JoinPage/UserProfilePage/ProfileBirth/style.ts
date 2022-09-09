8361f3f1bb32301809ae90029febe634f0122b82
NYeonK <skdus728@gmail.com>
2022-09-10 05:06:52 +0900

    [ design ] 변경된 로직에 맞게 다시 스타일링

--- src/@components/JoinPage/UserProfilePage/ProfileBirth/style.ts
+++ src/@components/JoinPage/UserProfilePage/ProfileBirth/style.ts
@@ -11,32 +11,15 @@ export const St = {
     margin: 1.2rem 0 2.4rem 0;
   `,
 
-  YearInputForm: styled.input`
-    width: 10.4rem;
+  BirthInputForm: styled.input`
+    width: 32.8rem;
+    padding-left: 2.4rem;
+    margin-left: -1.5rem;
 
     border: solid 0.1rem ${({ theme }) => theme.colors.gray300};
     border-radius: 4.9rem;
-  `,
-
-  MonthInputForm: styled.input`
-    width: 7.2rem;
-
-    border: solid 0.1rem ${({ theme }) => theme.colors.gray300};
-    border-radius: 4.9rem;
-  `,
-
-  DateInputForm: styled.input`
-    width: 7.2rem;
-
-    border: solid 0.1rem ${({ theme }) => theme.colors.gray300};
-    border-radius: 4.9rem;
-  `,
-
-  Text: styled.p`
-    margin: 1rem 0.8rem 0 1.8rem;
-
+    ${({ theme }) => theme.fonts.body6}
     color: ${({ theme }) => theme.colors.bg};
-    ${({ theme }) => theme.fonts.body6};
   `,
 
   Down: styled.div`
