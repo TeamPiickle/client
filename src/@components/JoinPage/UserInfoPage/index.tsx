f2996c0d0be1487b446323d0134baf767d503766
JY <pianowell96@gmail.com>
2022-09-10 15:35:44 +0900

    [ feat ] success버튼 컴포넌트 위치 수정

--- src/@components/JoinPage/UserInfoPage/index.tsx
+++ src/@components/JoinPage/UserInfoPage/index.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { prevPages } from "../../../core/join/prevPages";
 import { progressRate } from "../../../core/join/progressRate";
 import Footer from "../../@common/Footer";
 import Header from "../common/Header";
 import PageProgressBar from "../common/PageProgressBar";
-import SuccessBtn from "../common/SuccessBtn";
 import { St } from "./style";
 import UserEmail from "./UserEmail";
 import UserPassword from "./UserPassword";
 
 export default function UserInfo() {
+  const [isPasswordInvalid, setIsPasswordInvalid] = useState([false, false]);
+  const navigate = useNavigate();
+
+  const clickSuccessBtn = () => {
+    console.log(isPasswordInvalid);
+    if (isPasswordInvalid[0] === false && isPasswordInvalid[1] === false) {
+      // navigate();
+      console.log("okay");
+    }
+  };
+
   return (
     <>
       <Header prevPage={prevPages[2].prevPage} />
@@ -17,10 +30,10 @@ export default function UserInfo() {
         <St.UserInfoContainer>
           <St.ContentTitle>정보를 입력해주세요</St.ContentTitle>
           <UserEmail />
-          <UserPassword />
+          <UserPassword isPasswordInvalid={isPasswordInvalid} setIsPasswordInvalid={setIsPasswordInvalid} />
         </St.UserInfoContainer>
         <St.SuccessBtnContainer>
-          <SuccessBtn name="다음으로" />
+          <St.SuccessBtn onClick={clickSuccessBtn}>다음으로</St.SuccessBtn>
         </St.SuccessBtnContainer>
       </St.ContainerWrapper>
       <Footer />
--- src/@components/JoinPage/UserInfoPage/style.ts
+++ src/@components/JoinPage/UserInfoPage/style.ts
@@ -27,4 +27,18 @@ export const St = {
 
     width: 100%;
   `,
+
+  SuccessBtn: styled.button`
+    height: 5rem;
+    width: 100%;
+    /* width: 32.8rem; */
+
+    background-color: ${({ theme }) => theme.colors.black};
+
+    border-radius: 5rem;
+
+    color: ${({ theme }) => theme.colors.white};
+
+    ${({ theme }) => theme.fonts.btn2}
+  `,
 };
