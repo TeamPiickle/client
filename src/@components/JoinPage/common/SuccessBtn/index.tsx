28265b6231c09998f3846cdcbcabcef9a50f909a
JY <pianowell96@gmail.com>
2022-09-10 15:36:16 +0900

    [ feat ] 컴포넌트 삭제

--- src/@components/JoinPage/common/SuccessBtn/index.tsx
+++
@@ -1,9 +0,0 @@
-import { St } from "./style";
-
-interface ButtonContentsProps {
-  name: string;
-}
-
-export default function SuccessBtn({ name }: ButtonContentsProps) {
-  return <St.SuccessButton>{name}</St.SuccessButton>;
-}
