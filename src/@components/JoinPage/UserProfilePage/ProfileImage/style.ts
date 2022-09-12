56f619f44b74cd93b61c6c55dea5ec038578e833
NYeonK <skdus728@gmail.com>
2022-09-13 04:33:46 +0900

    [ design ] 업로드된 이미지 크기에 맞게 수정

--- src/@components/JoinPage/UserProfilePage/ProfileImage/style.ts
+++ src/@components/JoinPage/UserProfilePage/ProfileImage/style.ts
@@ -1,21 +1,34 @@
 import styled from "styled-components";
 
 export const St = {
-  ProfileImage: styled.section`
+  ProfileImage: styled.section``,
+
+  ImageContainer: styled.article`
+    position: relative;
     width: 8rem;
     height: 8rem;
-
-    position: relative;
     margin: 3.2rem auto;
     display: block;
   `,
 
-  AddImage: styled.img``,
+  ImageWrapper: styled.div`
+    width: 8rem;
+    height: 8rem;
+
+    border-radius: 70%;
+    overflow: hidden;
+  `,
+
+  AddImage: styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  `,
 
   AddBtnWrapper: styled.label`
     position: absolute;
     top: 5.8rem;
-    left: 6rem;
+    left: 5.8rem;
   `,
 
   AddButton: styled.input`
