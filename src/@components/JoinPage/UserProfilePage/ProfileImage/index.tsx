1914c72dae5a90c1a5d6bbddb64b28a361d5caf2
NYeonK <skdus728@gmail.com>
2022-09-13 04:31:13 +0900

    [ feat ] 프로필 사진 업로드 및 FormData로 넘겨주기

--- src/@components/JoinPage/UserProfilePage/ProfileImage/index.tsx
+++ src/@components/JoinPage/UserProfilePage/ProfileImage/index.tsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+
 import { IcAddProfileBtn } from "../../../../asset/icon";
 import { ImgDefaultBigProfile } from "../../../../asset/image";
 import { St } from "./style";
 
-export default function ProfileImage() {
+interface ImageTypes {
+  setImage: (file: FormData) => void;
+}
+
+export default function ProfileImage(props: ImageTypes) {
+  const { setImage } = props;
+  const [imgUrl, setImgUrl] = useState<string>("");
+
+  const handleImagePatch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files === null) return;
+
+    const selectedImg = e.target.files[0];
+    setImgUrl(URL.createObjectURL(selectedImg));
+
+    const formData = new FormData();
+    formData.append("file", selectedImg);
+    setImage(formData);
+  };
+
   return (
     <St.ProfileImage>
-      <St.AddImage src={ImgDefaultBigProfile} alt="프로필" />
-      <St.AddBtnWrapper>
-        <IcAddProfileBtn />
-        <St.AddButton type="file" accept="image/*" />
-      </St.AddBtnWrapper>
+      <St.ImageContainer>
+        <St.ImageWrapper>
+          <St.AddImage src={imgUrl ? imgUrl : ImgDefaultBigProfile} alt="프로필" />
+        </St.ImageWrapper>
+        <St.AddBtnWrapper>
+          <IcAddProfileBtn />
+          <St.AddButton type="file" onChange={handleImagePatch} accept="image/*" />
+        </St.AddBtnWrapper>
+      </St.ImageContainer>
     </St.ProfileImage>
   );
 }
