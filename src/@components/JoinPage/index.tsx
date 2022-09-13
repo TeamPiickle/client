bca0e81c762c9a091f34a184bfee287b4679dc30
JY <pianowell96@gmail.com>
2022-09-14 02:23:37 +0900

    [ feat ] useOutletContext 추가

--- src/@components/JoinPage/index.tsx
+++ src/@components/JoinPage/index.tsx
@@ -1,9 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import { Outlet, useOutletContext } from "react-router-dom";
+
+import { useOutletContexts } from "../../types/users";
 
 export default function JoinPage() {
+  const [userEmail, setUserEmail] = useState<useOutletContexts>();
+  const [userPassword, setUserPassword] = useState<useOutletContexts>();
   return (
     <div>
-      <Outlet />
+      <Outlet context={[userEmail, setUserEmail, userPassword, setUserPassword]} />
     </div>
   );
 }
