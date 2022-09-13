4c60e540298ec1459330ca9fbf2390528476193f
JY <pianowell96@gmail.com>
2022-09-14 02:24:50 +0900

    [ feat ] type 변경

--- src/@components/JoinPage/UserInfoPage/UserPassword/index.tsx
+++ src/@components/JoinPage/UserInfoPage/UserPassword/index.tsx
@@ -2,15 +2,20 @@ import { useEffect } from "react";
 
 import { St } from "./style";
 
+type invalidType = {
+  input: boolean;
+  confirm: boolean;
+};
+
 interface UserPasswordProps {
-  isPasswordInvalid: boolean[];
+  isPasswordInvalid: invalidType;
   checkInputInvalid: () => void;
   checkConfirmInvalid: () => void;
   debouncedQuery: string;
   changePasswordInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
   changePasswordConfirm: (e: React.ChangeEvent<HTMLInputElement>) => void;
   currentStep: string;
-  isUnfilled: boolean[];
+  isUnfilled: invalidType;
 }
 export default function UserPassword(props: UserPasswordProps) {
   const {
@@ -23,7 +28,6 @@ export default function UserPassword(props: UserPasswordProps) {
     currentStep,
     isUnfilled,
   } = props;
-
   useEffect(() => {
     if (currentStep === "input") {
       checkInputInvalid();
@@ -44,10 +48,10 @@ export default function UserPassword(props: UserPasswordProps) {
           type="password"
           placeholder="비밀번호 입력"
           onChange={changePasswordInput}
-          isFocused={isPasswordInvalid[0]}
+          isFocused={isPasswordInvalid.input}
         />
-        {isPasswordInvalid[0] &&
-          (isUnfilled[0] ? (
+        {isPasswordInvalid.input &&
+          (isUnfilled.input ? (
             <St.ErrorText>비밀번호를 입려해주세요</St.ErrorText>
           ) : (
             <St.ErrorText>영문, 숫자, 특수 문자를 하나 이상씩 조합해서 6자 이상으로 입력해주세요.</St.ErrorText>
@@ -58,9 +62,9 @@ export default function UserPassword(props: UserPasswordProps) {
           type="password"
           placeholder="비밀번호 확인"
           onChange={changePasswordConfirm}
-          isFocused={isPasswordInvalid[1]}
+          isFocused={isPasswordInvalid.confirm}
         />
-        {isPasswordInvalid[1] && <St.ErrorText>비밀번호가 일치하지 않습니다</St.ErrorText>}
+        {isPasswordInvalid.confirm && <St.ErrorText>비밀번호가 일치하지 않습니다</St.ErrorText>}
       </St.InputWrapper>
     </St.PasswordContainer>
   );
