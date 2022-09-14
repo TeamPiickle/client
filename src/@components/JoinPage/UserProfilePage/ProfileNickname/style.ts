0d589b6ed22351d2c2a7fb10db208d6e6feca747
NYeonK <skdus728@gmail.com>
2022-09-14 12:37:32 +0900

    [ refactor ] 닉네임 입력폼 calc() 수정

--- src/@components/JoinPage/UserProfilePage/ProfileNickname/style.ts
+++ src/@components/JoinPage/UserProfilePage/ProfileNickname/style.ts
@@ -1,37 +1,42 @@
-/* eslint-disable indent */
 import styled from "styled-components";
 
 export const St = {
   ProfileNickname: styled.section``,
 
   InputContainer: styled.div`
+    width: calc(100% - 3.2rem);
     height: 4.4rem;
+    margin: 1.2rem auto 2.4rem auto;
+
     display: flex;
     justify-content: center;
-    margin: 1.2rem 0 2.4rem 0;
   `,
 
   NickNameInputForm: styled.input<{ isincompletestate: boolean; iserrorstate: boolean }>`
-    width: calc(94% - 8.2rem);
+    width: calc(100% + 25rem);
     margin-right: 0.8rem;
     padding-left: 2.4rem;
+
     border: solid 0.1rem
       ${({ isincompletestate, theme }) => (isincompletestate ? theme.colors.red : theme.colors.gray300)};
     border-radius: 4.9rem;
     ${({ theme }) => theme.fonts.body6}
     color: ${({ iserrorstate, theme }) => (iserrorstate ? theme.colors.gray400 : theme.colors.bg)};
     background-color: ${({ iserrorstate, theme }) => (iserrorstate ? theme.colors.gray100 : theme.colors.white)};
+
     ${({ theme }) => theme.media.desktop`
-      width: 25.9rem;
+    width: 25.9rem;
     `};
   `,
 
   CheckBtn: styled.button`
-    width: calc(94% - 29.9rem);
+    width: calc(100% - 18rem);
+
     ${({ theme }) => theme.fonts.caption4};
     color: ${({ theme }) => theme.colors.bg};
     background-color: ${({ theme }) => theme.colors.gray300};
     border-radius: 3.8rem;
+
     ${({ theme }) => theme.media.desktop`
       width: 6.1rem;
     `};
@@ -39,12 +44,14 @@ export const St = {
 
   ErrorMessage: styled.div`
     margin: -2rem 4.2rem 1.6rem 2rem;
+
     color: ${({ theme }) => theme.colors.red};
     ${({ theme }) => theme.fonts.caption3}
   `,
 
   SuccessMessage: styled.div`
     margin: -2rem 4.2rem 1.6rem 2rem;
+
     color: ${({ theme }) => theme.colors.green};
     ${({ theme }) => theme.fonts.caption3}
   `,
