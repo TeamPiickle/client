8f53714fe5996543fd5afd99cf643ca7fe7e6266
NYeonK <skdus728@gmail.com>
2022-09-22 15:46:55 +0900

    [ feat ] 회원 탈퇴 PUT 함수 생성

---
+++ src/core/api/delete.ts
@@ -0,0 +1,10 @@
+import { realReq } from "./common/axios";
+import { PATH } from "./common/constants";
+
+function putDelete(reasons: string[]) {
+  return realReq.PUT(`${PATH.USERS_}/me`, reasons);
+}
+
+export const joinApi = {
+  putDelete,
+};
