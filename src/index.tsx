3199e912fca7b304db5e5e6fb26a78e27b0cf4d1
SNUPI <joohaem@daum.net>
2022-12-18 22:09:24 +0900

    feat: add GTM script by module

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import TagManager from "react-gtm-module";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 
 import App from "./App";
 import { GlobalStyle } from "./style/globalStyle";
 import theme from "./style/theme";
+
+const tagManagerArgs = {
+  gtmId: "GTM-KG6PFHT",
+};
+
+TagManager.initialize(tagManagerArgs);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
