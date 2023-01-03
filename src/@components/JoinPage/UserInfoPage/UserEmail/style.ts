236b8688d69a1a85fba39395b21b08832580acf6
NYeonK <skdus728@gmail.com>
2023-01-04 04:53:53 +0900

    [ design ] 이메일 input창 style 구현

--- src/@components/JoinPage/UserInfoPage/UserEmail/style.ts
+++ src/@components/JoinPage/UserInfoPage/UserEmail/style.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const St = {
-  EmailContainer: styled.article`
+  EmailContainer: styled.section`
     height: 7.4rem;
 
     display: flex;
@@ -11,33 +11,66 @@ export const St = {
     margin-bottom: 2.4rem;
   `,
 
-  EmailTitleWrapper: styled.h2`
+  EmailAuthenticationContent: styled.article`
+    height: 7.6rem;
     width: 100%;
-    ${({ theme }) => theme.fonts.body6};
-    color: ${({ theme }) => theme.colors.bg};
-    /* margin-left: 1.6rem; */
-  `,
-
-  EmailTitleText: styled.div`
-    margin-left: 0;
-  `,
-
-  EmailInputForm: styled.div`
-    height: 4.2rem;
-    width: 100%;
-
-    background-color: ${({ theme }) => theme.colors.gray100};
-
-    border-radius: 4.9rem;
-
-    margin-top: 1.2rem;
-    padding-left: 2.4rem;
 
     display: flex;
-    align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+  `,
 
-    color: ${({ theme }) => theme.colors.gray400};
+  ContentDescription: styled.h2`
+    ${({ theme }) => theme.fonts.body6}
+  `,
+
+  DescriptionContainer: styled.div`
+    display: flex;
 
     ${({ theme }) => theme.fonts.body6}
   `,
+
+  EssentialText: styled.div`
+    color: ${({ theme }) => theme.colors.bg};
+  `,
+
+  EssentialIcon: styled.div`
+    color: ${({ theme }) => theme.colors.red};
+  `,
+
+  InputContainer: styled.div`
+    height: 4.4rem;
+    margin-top: 1.2rem;
+
+    display: flex;
+  `,
+
+  EmailInput: styled.input`
+    width: 100%;
+
+    border: solid 0.1rem ${({ theme }) => theme.colors.gray300};
+    border-radius: 4.9rem;
+
+    margin-right: 0.8rem;
+    padding: 0 2.4rem;
+
+    ${({ theme }) => theme.fonts.body6}
+  `,
+
+  WarningText: styled.div`
+    width: 100%;
+
+    margin-left: 3.6rem;
+
+    ${({ theme }) => theme.fonts.caption3};
+    color: ${({ theme }) => theme.colors.red};
+  `,
 };
+
+/*
+  SendBtn: styled.button`
+    ${({ theme }) => theme.fonts.caption05}
+
+    text-decoration-line: underline;
+  `,
+*/
