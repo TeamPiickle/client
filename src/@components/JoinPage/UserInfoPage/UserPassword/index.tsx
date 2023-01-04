0a7abd6038ee2912c4a0c9fece2f430c280de982
NYeonK <skdus728@gmail.com>
2023-01-04 16:49:16 +0900

    [ etc ] 문장 끝의 점과 오타 수정

--- src/@components/JoinPage/UserInfoPage/UserPassword/index.tsx
+++ src/@components/JoinPage/UserInfoPage/UserPassword/index.tsx
@@ -51,9 +51,9 @@ export default function UserPassword(props: UserPasswordProps) {
         />
         {isPasswordInvalid.input &&
           (isUnfilled.input ? (
-            <St.ErrorText>비밀번호를 입려해주세요</St.ErrorText>
+            <St.ErrorText>비밀번호를 입력해주세요</St.ErrorText>
           ) : (
-            <St.ErrorText>영문, 숫자, 특수 문자를 하나 이상씩 조합해서 6자 이상으로 입력해주세요.</St.ErrorText>
+            <St.ErrorText>영문, 숫자, 특수 문자를 하나 이상씩 조합해서 6자 이상으로 입력해주세요</St.ErrorText>
           ))}
       </St.InputWrapper>
       <St.InputWrapper>
