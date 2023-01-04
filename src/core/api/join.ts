8937a409bb9b8800f1f0d34e9b699cb0f257e1b0
NYeonK <skdus728@gmail.com>
2023-01-04 17:03:42 +0900

    [ refactor ] 불필요한 api 및 path 제거

--- src/core/api/join.ts
+++ src/core/api/join.ts
@@ -15,20 +15,11 @@ function fetchNickNameValid<T>(nickname: string) {
   return realReq.GET<T>(`${PATH.USERS_}/nickname/is-exist?${nicknameParams}`);
 }
 
-async function postEmail(email: string) {
-  const postingEmail = {
-    email,
-  };
-
-  return await realReq.POST(`${PATH.USERS_}${PATH.USERS_EMAIL}`, postingEmail);
-}
-
 function postUserInfo(postingUserInfo: UserInfo) {
   return realReq.POST(PATH.USERS_, postingUserInfo);
 }
 
 export const joinApi = {
-  postEmail,
   postUserInfo,
   postJoin,
   fetchNickNameValid,
