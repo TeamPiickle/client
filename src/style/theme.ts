db399602a4a8f588ecfe7dd81b4a16686850ae51
NYeonK <skdus728@gmail.com>
2023-01-23 20:04:32 +0900

    [ fix ] theme.ts 잘못된 값 수정

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -84,7 +84,7 @@ const newFonts = {
   h2: FONT({ weight: 400, size: 1.4, lineHeight: 140, letterSpacing: 3 }),
   body1: FONT({ weight: 600, size: 2.4, lineHeight: 140, letterSpacing: 3 }),
   body2: FONT({ weight: 600, size: 2.0, lineHeight: 140, letterSpacing: 0 }),
-  body3: FONT({ weight: 500, size: 1.8, lineHeight: 140, letterSpacing: 3.5 }),
+  body3: FONT({ weight: 500, size: 1.8, lineHeight: 140, letterSpacing: 35 }),
   btn1: FONT({ weight: 400, size: 1.6, lineHeight: 140, letterSpacing: 4 }),
   btn2: FONT({ weight: 400, size: 1.2, lineHeight: 140, letterSpacing: 4 }),
   caption1: FONT({ weight: 300, size: 1.2, lineHeight: 140, letterSpacing: 3 }),
