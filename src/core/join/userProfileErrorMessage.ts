76b863c5bd9d918e4d02efb1fbf8cc31e15e3f72
SNUPI <joohaem@daum.net>
2023-02-28 23:47:12 +0900

    [ etc ] join alert key 주석 제거

--- src/core/join/userProfileErrorMessage.ts
+++ src/core/join/userProfileErrorMessage.ts
@@ -2,13 +2,10 @@ export const JOIN_PROFILE_ALERT_KEY = {
   Okay: "",
   nickName: {
     input: "nickNameInput",
-    // check: "닉네임 중복 확인을 해주세요",
     valid: "nickNameValid",
     fail: "nickNameFail",
-    // success: "사용가능한 닉네임입니다",
   },
   birth: {
-    // input: "생년월일을 입력해주세요",
     check: "birthCheck",
     valid: "birthValid",
   },
