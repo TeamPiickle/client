48e3c43714b128f7e5f7c0b1dba388074b89265e
NYeonK <skdus728@gmail.com>
2023-03-09 05:46:54 +0900

    [ feat ] Preview에 close 버튼 추가 및 카드 보기 클릭 시, 카드뷰로 이동

--- src/@components/MainPage/Medly/MedleyPreview/index.tsx
+++ src/@components/MainPage/Medly/MedleyPreview/index.tsx
@@ -1,14 +1,36 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
 
+import { IcCloseBtn } from "../../../../asset/icon";
 import { useMedleyLists } from "../../../../core/api/main";
+import { sliderIdxState } from "../../../../core/atom/slider";
+import { routePaths } from "../../../../core/routes/path";
+import useOutClickCloser from "../../../@common/hooks/useOutClickCloser";
 import St from "./style";
 
-export default function MedleyPreview() {
+interface MedleyPreviewProps {
+  closePreview: () => void;
+}
+
+export default function MedleyPreview(props: MedleyPreviewProps) {
+  const { closePreview } = props;
+  const outClickCloserRef = useOutClickCloser(closePreview);
   const { medleyLists } = useMedleyLists();
+  const setSliderIdx = useSetRecoilState(sliderIdxState);
+  const navigate = useNavigate();
+
+  const moveMedley = (id: string) => {
+    navigate(routePaths.CardCollection, { state: { type: "medley", medleyId: id } });
+    setSliderIdx(0);
+  };
 
   return (
     <St.Root>
-      <St.Container>
+      <St.Container ref={outClickCloserRef}>
+        <St.CloseBtnContainer onClick={closePreview}>
+          <IcCloseBtn />
+        </St.CloseBtnContainer>
         {medleyLists &&
           medleyLists.data.map((medleyLists) => {
             return (
@@ -21,7 +43,7 @@ export default function MedleyPreview() {
                     return <St.CardWrapper key={i}>{medleyLists.previewCards[i].content}</St.CardWrapper>;
                   })}
                 </St.PreviewCards>
-                <St.MoveBtn>카드 보기</St.MoveBtn>
+                <St.MoveBtn onClick={() => moveMedley(medleyLists._id)}>카드 보기</St.MoveBtn>
               </St.MedleyPreview>
             );
           })}
