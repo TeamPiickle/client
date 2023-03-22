b2edd1bfd934993e86e9a536cc96c5fe02dbf94b
NYeonK <skdus728@gmail.com>
2023-03-23 01:58:37 +0900

    [ feat ] medley banner 및 title, tag 추가

--- src/@components/MainPage/Medley/style.ts
+++ src/@components/MainPage/Medley/style.ts
@@ -1,10 +1,42 @@
 import styled from "styled-components";
 
-const Container = styled.section``;
+import { ImgMedleyBanner } from "../../../asset/image";
+import { OriginImgMedleyBanner } from "../../../asset/image/origin";
+
+const Container = styled.section`
+  height: 39.8rem;
+
+  margin-top: 2.4rem;
+  padding-top: 4rem;
+
+  background-image: url(${ImgMedleyBanner});
+  background-image: image-set(${ImgMedleyBanner} type("image/webp"), ${OriginImgMedleyBanner} type("image/png"));
+  background-size: cover;
+`;
+
+const TitleTag = styled.p`
+  width: 17rem;
+
+  padding: 0rem 0.4rem;
+  margin: 0.8rem 0 0 1.6rem;
+
+  ${({ theme }) => theme.newFonts.caption1}
+  color: ${({ theme }) => theme.newColors.gray900};
+  background-color: ${({ theme }) => theme.newColors.green};
+`;
 
 const Title = styled.h1`
-  height: 5.2rem;
-  padding: 1.6rem;
+  margin-left: 1.6rem;
+
+  ${({ theme }) => theme.newFonts.body1}
+  color: ${({ theme }) => theme.newColors.gray900};
+`;
+
+const SubTitle = styled.p`
+  height: 6.6rem;
+
+  padding: 2.4rem 0;
+  margin-left: 1.6rem;
 
   ${({ theme }) => theme.newFonts.h2}
   color: ${({ theme }) => theme.newColors.gray900};
@@ -26,7 +58,9 @@ const Medley = styled.article`
 
 const St = {
   Container,
+  TitleTag,
   Title,
+  SubTitle,
   Medley,
 };
 export default St;
