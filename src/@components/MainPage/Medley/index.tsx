fce9e1765bfb42567cd8de4df338e14c58bc6c26
SNUPI <joohaem@daum.net>
2023-03-24 00:52:39 +0900

    feat: 메들리 배너사진 프리로드

--- src/@components/MainPage/Medley/index.tsx
+++ src/@components/MainPage/Medley/index.tsx
@@ -1,3 +1,6 @@
+import { Helmet } from "react-helmet";
+
+import { ImgMedleyBanner } from "../../../asset/image";
 import useScrollableContainer from "../../@common/hooks/useScrollableContainer";
 import Loading from "../../@common/Loading";
 import { useMedleyLists } from "../hooks/useMedleyLists";
@@ -10,18 +13,23 @@ export default function Medley() {
 
   if (isLoading) return <Loading backgroundColor="white" />;
   return (
-    <St.Container>
-      <St.TitleTag>💥 NEW 💥 새로운 기능 업데이트</St.TitleTag>
-      <St.Title>
-        새로운 대화카드 주제
-        <br />
-        업데이트
-      </St.Title>
-      <St.SubTitle>👇 나에게 딱 맞는 주제, 피클이 대신 골라드립니다 👇</St.SubTitle>
-      <St.Medley {...scrollableContainerProps}>
-        {randomMedleyLists &&
-          randomMedleyLists.map((medleyCard) => <MedleyCard key={medleyCard._id} medleyCard={medleyCard} />)}
-      </St.Medley>
-    </St.Container>
+    <>
+      <Helmet>
+        <link rel="preload" as="image" href={ImgMedleyBanner} />
+      </Helmet>
+      <St.Container>
+        <St.TitleTag>💥 NEW 💥 새로운 기능 업데이트</St.TitleTag>
+        <St.Title>
+          새로운 대화카드 주제
+          <br />
+          업데이트
+        </St.Title>
+        <St.SubTitle>👇 나에게 딱 맞는 주제, 피클이 대신 골라드립니다 👇</St.SubTitle>
+        <St.Medley {...scrollableContainerProps}>
+          {randomMedleyLists &&
+            randomMedleyLists.map((medleyCard) => <MedleyCard key={medleyCard._id} medleyCard={medleyCard} />)}
+        </St.Medley>
+      </St.Container>
+    </>
   );
 }
