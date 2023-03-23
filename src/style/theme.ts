253f7a976a51242cec99018dd00ca98d010c5f2f
NYeonK <skdus728@gmail.com>
2023-03-23 21:06:28 +0900

    [ feat ] theme에 new font 추가

--- src/style/theme.ts
+++ src/style/theme.ts
@@ -106,11 +106,10 @@ const newFonts = {
   btn1: FONT({ weight: 400, size: 1.6, lineHeight: 140, letterSpacing: 4 }),
   btn2: FONT({ weight: 400, size: 1.2, lineHeight: 140, letterSpacing: 4 }),
   caption1: FONT({ weight: 300, size: 1.2, lineHeight: 140, letterSpacing: 3 }),
+  caption2: FONT({ weight: 400, size: 1.0, lineHeight: 140, letterSpacing: 4 }),
   footer1: FONT({ weight: 600, size: 1.4, lineHeight: 140, letterSpacing: 4 }),
   footer2: FONT({ weight: 400, size: 1.4, lineHeight: 130, letterSpacing: 4 }),
   graphic1: FONT({ weight: 600, size: 2.6, lineHeight: 130, letterSpacing: 3 }),
-  test1: FONT({ weight: 700, size: 1.4, lineHeight: 140, letterSpacing: 4 }),
-  test2: FONT({ weight: 400, size: 1.0, lineHeight: 140, letterSpacing: 4 }),
 } as const;
 
 type BackQuoteArgs = string[];
