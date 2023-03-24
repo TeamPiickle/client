db3ca119c33572443305c5155b21c77ce1308f3c
NYeonK <skdus728@gmail.com>
2023-03-25 00:17:57 +0900

    [ feat ] LoginPage로 이동 시, scroll를 맨 위로 고정

--- src/@components/LoginPage/index.tsx
+++ src/@components/LoginPage/index.tsx
@@ -1,10 +1,12 @@
 import Footer from "../@common/Footer";
 import Header from "../@common/Header";
 import useGTMPage from "../@common/hooks/useGTMPage";
+import useScroll from "../@common/hooks/useScroll";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   useGTMPage();
+  useScroll();
 
   return (
     <main>
