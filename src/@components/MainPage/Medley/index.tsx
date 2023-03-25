17a9ca06fe241eb0b7854faf91ae319abb2eff5c
NYeonK <skdus728@gmail.com>
2023-03-25 17:21:26 +0900

    [ feat ] Medley text 수정

--- src/@components/MainPage/Medley/index.tsx
+++ src/@components/MainPage/Medley/index.tsx
@@ -18,13 +18,13 @@ export default function Medley() {
         <link rel="preload" as="image" href={ImgMedleyBanner} />
       </Helmet>
       <St.Container>
-        <St.TitleTag>💥 NEW 💥 새로운 기능 업데이트</St.TitleTag>
+        <St.TitleTag>NEW 새로운 기능 업데이트</St.TitleTag>
         <St.Title>
-          새로운 대화카드 주제
+          대화주제 메들리
           <br />
           업데이트
         </St.Title>
-        <St.SubTitle>👇 나에게 딱 맞는 주제, 피클이 대신 골라드립니다 👇</St.SubTitle>
+        <St.SubTitle>👇 나에게 딱 맞는 주제, 피클이 대신 골라드립니다</St.SubTitle>
         <St.Medley {...scrollableContainerProps}>
           {randomMedleyLists &&
             randomMedleyLists.map((medleyCard) => <MedleyCard key={medleyCard._id} medleyCard={medleyCard} />)}
