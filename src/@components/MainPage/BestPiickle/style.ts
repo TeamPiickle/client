1379db3a60033e884db813fc2c066a87107a72dc
SNUPI <joohaem@daum.net>
2023-03-25 22:59:39 +0900

    hotfix: best piickle margin value

--- src/@components/MainPage/BestPiickle/style.ts
+++ src/@components/MainPage/BestPiickle/style.ts
@@ -16,10 +16,6 @@ const SliderWrapper = styled.article`
     margin-right: 1.6rem;
   }
 
-  & > button:not(:first-child) {
-    margin-left: 0.8rem;
-  }
-
   -ms-overflow-style: none; /* IE and Edge */
   scrollbar-width: none; /* Firefox */
   &::-webkit-scrollbar {
