433708fd30428f8166a35bdf04b9e107d000079e
NYeonK <skdus728@gmail.com>
2023-04-06 01:32:32 +0900

    [ feat ] event 전파 방지 메소드 관련 props 추가

--- src/@components/@common/hooks/useOutClickCloser.ts
+++ src/@components/@common/hooks/useOutClickCloser.ts
@@ -2,15 +2,22 @@ import { useEffect, useRef } from "react";
 
 type HandleOutClickCloser = () => void;
 
-export default function useOutClickCloser(handleOutClickCloser: HandleOutClickCloser) {
+interface useOutClickCloserProps {
+  preventEventPropagation: boolean;
+}
+
+export default function useOutClickCloser(handleOutClickCloser: HandleOutClickCloser, props: useOutClickCloserProps) {
+  const { preventEventPropagation } = props;
   const currentRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const handleClickOutside = (e: MouseEvent | TouchEvent) => {
       if (!(e.target instanceof HTMLElement)) return;
       if (currentRef.current && !currentRef.current.contains(e.target)) {
-        e.preventDefault();
-        e.stopPropagation();
+        if (preventEventPropagation) {
+          e.preventDefault();
+          e.stopPropagation();
+        }
         handleOutClickCloser();
       }
     };
@@ -22,7 +29,7 @@ export default function useOutClickCloser(handleOutClickCloser: HandleOutClickCl
       document.removeEventListener("mousedown", handleClickOutside);
       document.removeEventListener("touchstart", handleClickOutside);
     };
-  }, [currentRef, handleOutClickCloser]);
+  }, [currentRef, handleOutClickCloser, preventEventPropagation]);
 
   return currentRef;
 }
