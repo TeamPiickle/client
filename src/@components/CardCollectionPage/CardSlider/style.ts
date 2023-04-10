00783f3c2163ca89bde8fbde8626e2af41891adc
NYeonK <skdus728@gmail.com>
2023-04-11 01:21:10 +0900

    [ fix ] 모바일에서 스와이프 시, 하단에 생기는 여백 문제 해결

--- src/@components/CardCollectionPage/CardSlider/style.ts
+++ src/@components/CardCollectionPage/CardSlider/style.ts
@@ -6,6 +6,8 @@ const Wrapper = styled.section`
   position: relative;
   height: 100%;
 
+  padding-top: 5.2rem;
+
   & .swiper {
     width: 100%;
     height: 100%;
--- src/@components/CardCollectionPage/style.ts
+++ src/@components/CardCollectionPage/style.ts
@@ -6,8 +6,6 @@ export const St = {
     height: calc(var(--vh, 1vh) * 100);
     min-height: -webkit-fill-available;
 
-    padding-top: 5.2rem;
-
     background: ${({ theme }) => theme.colors.card};
   `,
 };
