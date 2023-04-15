1b3af163be5a5e284df1fde2a1e004afdd651b0d
SNUPI <joohaem@daum.net>
2023-04-15 18:57:08 +0900

    chore: config 주 제거

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()], //https://react-svgr.com/docs/options/
+  plugins: [react(), svgr()],
   base: "",
 });
