c647cd95d4b35997eff4e949bd88824c2cb6bf68
NYeonK <skdus728@gmail.com>
2023-04-16 18:17:10 +0900

    [ feat ] LastCard UI 구현 및 스타일링

--- src/@components/CardCollectionPage/Card/LastCard/style.ts
+++ src/@components/CardCollectionPage/Card/LastCard/style.ts
@@ -1,47 +1,21 @@
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import { St as StCard } from "../style";
-
 export const St = {
-  Card: styled(StCard.Card)``,
+  Card: styled.section`
+    margin-bottom: 1.6rem;
+  `,
 
-  ContentTitle: styled(StCard.ContentWrapper)`
-    margin-top: 8.6rem;
+  ContentWrapper: styled.div`
+    padding: 2.4rem 1.6rem;
+  `,
 
-    ${({ theme }) => theme.fonts.body5};
+  ContentTitle: styled.strong`
+    ${({ theme }) => theme.newFonts.h1};
+    color: ${({ theme }) => theme.newColors.gray900};
   `,
 
   Content: styled.p`
-    margin: 1.2rem 0 0 2.4rem;
-
-    ${({ theme }) => theme.fonts.body6};
-    color: ${({ theme }) => theme.colors.bg};
-  `,
-
-  CategoryLink: styled(Link)`
-    position: absolute;
-    top: 50%;
-    right: 0;
-    transform: translate(50%, -50%);
-  `,
-
-  HomeLink: styled(Link)`
-    position: absolute;
-    bottom: 3.2rem;
-    left: 50%;
-    transform: translateX(-50%);
-
-    width: 8.3rem;
-    height: 1.9rem;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    ${({ theme }) => theme.fonts.btn1};
-    color: ${({ theme }) => theme.colors.gray600};
-
-    border-bottom: 0.08rem solid ${({ theme }) => theme.colors.gray600};
+    ${({ theme }) => theme.newFonts.h2};
+    color: ${({ theme }) => theme.newColors.gray900};
   `,
 };
