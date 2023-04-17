c552798eb17dd8418cec8c7d57c569f782306aa7
NYeonK <skdus728@gmail.com>
2023-04-18 01:33:24 +0900

    [ refactor ] LastCard component 정리

--- src/@components/CardCollectionPage/Card/LastCard/style.ts
+++ src/@components/CardCollectionPage/Card/LastCard/style.ts
@@ -5,16 +5,16 @@ export const St = {
     margin-bottom: 1.6rem;
   `,
 
-  ContentWrapper: styled.div`
-    padding: 2.4rem 1.6rem;
-  `,
+  ContentTitle: styled.h1`
+    padding: 2.4rem 1.6rem 0;
 
-  ContentTitle: styled.strong`
     ${({ theme }) => theme.newFonts.h1};
     color: ${({ theme }) => theme.newColors.gray900};
   `,
 
   Content: styled.p`
+    padding: 0 1.6rem 2.4rem;
+
     ${({ theme }) => theme.newFonts.h2};
     color: ${({ theme }) => theme.newColors.gray900};
   `,
