a114271e2defdadbb27e05562ed3ba86b1d7a783
NYeonK <skdus728@gmail.com>
2023-04-23 22:02:21 +0900

    [ feat ] 로그인 여부에 따라, header에 MY 피클 버튼 띄우기

--- src/@components/@common/Header/HeaderMinVer/index.tsx
+++ src/@components/@common/Header/HeaderMinVer/index.tsx
@@ -2,9 +2,11 @@ import { routePaths } from "../../../../core/routes/path";
 import St from "./style";
 
 export default function HeaderMinVer() {
+  const LOGIN_STATE = localStorage.getItem("piickle-token") ? true : false;
+
   return (
     <St.Header>
-      <St.Link to={routePaths.BookmarkPage}>MY 피클</St.Link>
+      {LOGIN_STATE && <St.Link to={routePaths.BookmarkPage}>MY 피클</St.Link>}
       <St.Link to={routePaths.Main}>홈으로 가기</St.Link>
     </St.Header>
   );
