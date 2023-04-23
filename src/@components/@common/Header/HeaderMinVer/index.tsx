b3d267b567e05a03056d83e00d44f1a99a712618
NYeonK <skdus728@gmail.com>
2023-04-23 21:48:37 +0900

    [ feat ] card header text 및 link 변경

--- src/@components/@common/Header/HeaderMinVer/index.tsx
+++ src/@components/@common/Header/HeaderMinVer/index.tsx
@@ -4,8 +4,8 @@ import St from "./style";
 export default function HeaderMinVer() {
   return (
     <St.Header>
-      <St.Link to={routePaths.BookmarkPage}>마이 피클 리스트 보기</St.Link>
-      <St.Link to={routePaths.Category}>대화 카테고리 보기</St.Link>
+      <St.Link to={routePaths.BookmarkPage}>MY 피클</St.Link>
+      <St.Link to={routePaths.Main}>홈으로 가기</St.Link>
     </St.Header>
   );
 }
