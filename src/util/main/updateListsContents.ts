a81c878130815ed5931fbbdcac0946a5de91a3c8
NYeonK <skdus728@gmail.com>
2023-04-30 04:50:30 +0900

    [ feat ] Add update contents array

---
+++ src/util/main/updateListsContents.ts
@@ -0,0 +1,6 @@
+export const updateListsContents = [
+  { content: "카드 넘기기가 세로 스크롤로 변경 되었어요!" },
+  { content: "북마크 이모티콘이 변경 되었어요!" },
+  { content: "19금 대화주제 필터를 오픈했어요!" },
+  { content: "새로운 대화주제가 추가되었어요!" },
+];
