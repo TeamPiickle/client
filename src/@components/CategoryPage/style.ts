ff771d4b58fb0b950d1a52e07c3ff16b73dfb5d2
NYeonK <skdus728@gmail.com>
2023-05-10 02:12:52 +0900

    [ design ] categoryPage 상하여백 수정

--- src/@components/CategoryPage/style.ts
+++ src/@components/CategoryPage/style.ts
@@ -3,8 +3,7 @@ import styled from "styled-components";
 const Root = styled.main`
   display: flex;
   flex-direction: column;
-  margin-bottom: 4rem;
-  margin-top: -2.4rem;
+  padding-bottom: 4rem;
 `;
 
 const CategoryBanner = styled.section`
