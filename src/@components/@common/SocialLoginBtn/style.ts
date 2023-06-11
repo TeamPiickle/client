3e13bf9726099360a58f612b94bcfa30c67ec764
NYeonK <skdus728@gmail.com>
2023-06-11 22:11:29 +0900

    [ design ] add social login button style

---
+++ src/@components/@common/SocialLoginBtn/style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const LoginBtn = styled.div<{ bgColor: string }>`
+  width: 32.8rem;
+  height: 5rem;
+
+  margin-top: 0.8rem;
+
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme, bgColor }) => (bgColor === "KAKAO" ? theme.newColors.red : theme.newColors.green)};
+`;
+
+export const BtnLogo = styled.div``;
+
+export const BtnText = styled.p`
+  ${({ theme }) => theme.newFonts.h1};
+`;
