d3d6b8474a5d41b630ec31a2d2a64be3f381b986
NYeonK <skdus728@gmail.com>
2023-06-15 03:37:44 +0900

    [ feat ] update env file for social login

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,10 @@
 interface ImportMetaEnv {
   readonly VITE_BASE_URL: string;
+  readonly VITE_KAKAO_CLIENT_ID: string;
+  readonly VITE_KAKAO_REDIRECT_URI: string;
+  readonly VITE_NAVER_CLIENT_ID: string;
+  readonly VITE_NAVER_REDIRECT_URI: string;
+  readonly VITE_NAVER_STATE: string;
   readonly DEV: boolean;
 }
 
