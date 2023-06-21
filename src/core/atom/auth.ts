c73bd66f9ae8802bdb07189fcc11441152fe2759
SNUPI <joohaem@daum.net>
2023-06-21 15:21:48 +0900

    fix: Auth Initial Header

--- src/core/atom/auth.ts
+++ src/core/atom/auth.ts
@@ -11,6 +11,10 @@ export const userTokenAtom = atom<string | null>({
   key: StateType.USER_TOKEN_ATOM,
   default: localStorage.getItem(USER_TOKEN) || null,
   effects: [
+    () => {
+      const initialToken = localStorage.getItem(USER_TOKEN);
+      if (initialToken) axiosInstance.defaults.headers.common["x-auth-token"] = `Bearer ${initialToken}`;
+    },
     ({ onSet }) => {
       onSet((newToken, _, isReset) => {
         if (isReset || newToken === null) {
