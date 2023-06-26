ded49f454e1e8b346f8f94f16f9f76ce3ad11cbf
NYeonK <skdus728@gmail.com>
2023-06-26 14:57:26 +0900

    [ feat ] update env file

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_KAKAO_REDIRECT_URI: string;
   readonly VITE_NAVER_CLIENT_ID: string;
   readonly VITE_NAVER_REDIRECT_URI: string;
+  readonly VITE_NAVER_CLIENT_SECRET: string;
   readonly VITE_NAVER_STATE: string;
   readonly DEV: boolean;
 }
