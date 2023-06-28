d2823621fbc8e6eea5a09fc1efebc91c519402af
NYeonK <skdus728@gmail.com>
2023-06-29 02:57:09 +0900

    [ feat ] successPage 기본 레이아웃 및 스타일 구현

--- src/@components/OAuthPage/SuccessPage/index.tsx
+++ src/@components/OAuthPage/SuccessPage/index.tsx
@@ -1,5 +1,21 @@
+import { IcLeftSpeechBubble, IcRightSpeechBubble } from "../../../asset/icon";
+import { routePaths } from "../../../core/routes/path";
+import Header from "./../../@common/Header";
 import * as St from "./style";
 
 export default function SuccessPage() {
-  return <St.Container>success</St.Container>;
+  return (
+    <St.SuceessPage>
+      <Header />
+      <IcLeftSpeechBubble />
+      <IcRightSpeechBubble />
+      <St.WelcomeText>
+        피클님 반가워요!
+        <br />
+        즐거운 대화를 나누러 가볼까요?
+      </St.WelcomeText>
+      <St.GoHomeBtn to={routePaths.Main}>홈으로 가기</St.GoHomeBtn>
+      <St.GoMypageBtn to={routePaths.MyPage}>프로필 설정하기</St.GoMypageBtn>
+    </St.SuceessPage>
+  );
 }
--- src/@components/OAuthPage/SuccessPage/style.ts
+++ src/@components/OAuthPage/SuccessPage/style.ts
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export const Container = styled.main`
-  background: linear-gradient(180deg, #dbfff1 0%, #ffffeb 100%);
+export const SuceessPage = styled.main`
+  height: 100vh;
+
+  background: ${({ theme }) => theme.colors.card};
 `;
+
+export const WelcomeText = styled.p`
+  text-align: center;
+
+  ${({ theme }) => theme.newFonts.h1};
+`;
+
+export const GoHomeBtn = styled(Link)``;
+
+export const GoMypageBtn = styled(Link)``;
