4df350db887bcbe253c67eff108326f79efa710e
Eunhyung Choi <milkiss0414@naver.com>
2023-07-17 20:22:34 +0900

    [ feat ] GET useRecentlyUpdated hook

--- src/@components/@common/hooks/useRecentlyUpdated.ts
+++ src/@components/@common/hooks/useRecentlyUpdated.ts
@@ -5,13 +5,13 @@ import { PATH } from "../../../core/api/common/constants";
 import { RecentCardList } from "../../../types/cardCollection";
 import { PiickleSWRResponse } from "../../../types/remote/swr";
 
-export function useRecentlyBookmarked() {
-  const { data } = useSWR<PiickleSWRResponse<RecentCardList>>(`${PATH.CARDS_}${PATH.CARDS_RECENT}`, realReq.GET_SWR, {
+export function useRecentlyUpdated() {
+  const { data } = useSWR<PiickleSWRResponse<RecentCardList>>(`${PATH.CARDS_}${PATH.CARDS_UPDATE}`, realReq.GET_SWR, {
     suspense: true,
   });
 
   return {
-    recentlyBookmarkedDate: data?.data.data.recentlyDate,
-    recentlyBookmarkedCards: data?.data.data.cardResponseDtos,
+    recentlyUpdatedDate: data?.data.data.recentlyDate,
+    recentlyUpdatedCards: data?.data.data.cardResponseDtos,
   };
 }
--- src/@components/MainPage/Banner/index.tsx
+++ src/@components/MainPage/Banner/index.tsx
@@ -6,6 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { CardList } from "../../../types/cardCollection";
 import { BannerImage, newBannerImages } from "../../../util/main/banner";
 import { useRecentlyBookmarked } from "../../@common/hooks/useRecentlyBookmarked";
+import { useRecentlyUpdated } from "../../@common/hooks/useRecentlyUpdated";
 import useBannerSwiper from "../hooks/useBannerSwiper";
 import St from "./style";
 
@@ -20,14 +21,15 @@ interface newBannerType {
 
 export default function Banner() {
   const { swiperSettings, currentSlide } = useBannerSwiper();
-  const { recentlyDate, recentlyBookmarkedCards } = useRecentlyBookmarked();
+  const { recentlyBookmarkedDate, recentlyBookmarkedCards } = useRecentlyBookmarked();
+  const { recentlyUpdatedDate, recentlyUpdatedCards } = useRecentlyUpdated();
 
   const newBanners: Partial<newBannerType>[] = [
     {
       bannerImage: newBannerImages[0],
       phrase: "가장 최근 북마크 된",
       topic: "핫한 대화주제",
-      date: recentlyDate,
+      date: recentlyBookmarkedDate,
       cards: recentlyBookmarkedCards,
       linkTo: "",
     },
@@ -35,16 +37,14 @@ export default function Banner() {
       bannerImage: newBannerImages[1],
       phrase: "가장 최근 업데이트 된",
       topic: "새로운 대화주제",
-      date: "",
-      cards: [],
+      date: recentlyUpdatedDate,
+      cards: recentlyUpdatedCards,
       linkTo: "",
     },
     {
       bannerImage: newBannerImages[2],
       phrase: "여러분만의 톡톡 튀는",
       topic: "대화주제를 얘기해주세요",
-      date: "",
-      cards: [],
       linkTo: "",
     },
   ];
