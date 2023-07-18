9333387473d0dff09640844262eb2244bbe2534d
NYeonK <skdus728@gmail.com>
2023-07-18 23:58:44 +0900

    [ feat ] change last card ui

--- src/@components/CardCollectionPage/Card/LastCard/index.tsx
+++ src/@components/CardCollectionPage/Card/LastCard/index.tsx
@@ -1,14 +1,19 @@
 import React, { forwardRef } from "react";
 
-import CategoryContents from "../../../CategoryPage/CategoryContents";
-import St from "./style";
+import { IcCongratPiickle } from "../../../../asset/icon";
+import { routePaths } from "../../../../core/routes/path";
+import * as St from "./style";
 
 const LastCard = forwardRef(function LastCard(_, ref: React.ForwardedRef<HTMLDivElement>) {
   return (
     <St.Card ref={ref}>
       <St.ContentTitle>의미있는 대화를 나누셨나요?</St.ContentTitle>
-      <St.Content>끊임없는 대화를 위해 대화주제를 선택해주세요</St.Content>
-      <CategoryContents />
+      <St.Content>끊임없는 대화를 위해 버튼을 선택해주세요</St.Content>
+      <IcCongratPiickle />
+      <St.BtnContainer>
+        <St.ReplayBtn to={""}>비슷한 주제 계속 보기</St.ReplayBtn>
+        <St.CategoryBtn to={routePaths.Category}>카테고리 보러 가기</St.CategoryBtn>
+      </St.BtnContainer>
     </St.Card>
   );
 });
