16a34cd112b007c3aab06cc7e06f4af5abb984bf
Eunhyung Choi <milkiss0414@naver.com>
2023-07-26 21:00:17 +0900

    [ fix ] timer 여러개 생성시 버그

--- src/@components/@common/Toast/ToastProvider.tsx
+++ src/@components/@common/Toast/ToastProvider.tsx
@@ -1,6 +1,7 @@
 import React, { PropsWithChildren, useCallback, useState } from "react";
 
 import { ToastContext } from "./context";
+import useTimeout from "./hooks/useTimeout";
 import * as St from "./style";
 
 export type ToastType = {
@@ -10,20 +11,17 @@ export type ToastType = {
 
 export default function ToastProvider({ children }: PropsWithChildren) {
   const [toast, setToast] = useState<ToastType | null>(null);
+  const toastTimeout = useTimeout();
 
   const showToast = useCallback(
     async ({ message, duration }: ToastType) => {
       setToast({ message, duration });
 
-      const timer: ReturnType<typeof setTimeout> = await new Promise(() =>
-        setTimeout(() => {
-          setToast(null);
-        }, duration * 1000),
-      );
-
-      clearTimeout(timer);
+      toastTimeout.set(() => {
+        setToast(null);
+      }, duration * 1000);
     },
-    [setToast],
+    [setToast, toastTimeout],
   );
 
   return (
