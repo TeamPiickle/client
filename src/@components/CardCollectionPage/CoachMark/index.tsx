1d32ad2ac28076cbfb39ba3d94094caebd8680f3
NYeonK <skdus728@gmail.com>
2023-08-14 23:48:40 +0900

    [ feat ] create CoachMark component

---
+++ src/@components/CardCollectionPage/CoachMark/index.tsx
@@ -0,0 +1,35 @@
+import IcMenuBtn from "../../../asset/icon/IcMenuBtn";
+import IcShareBtn from "../../../asset/icon/IcShareBtn";
+import Modal from "../../@common/Modal";
+import * as St from "./style";
+
+interface CoachMarkProps {
+  closeHandler: () => void;
+}
+
+export default function CoachMark(props: CoachMarkProps) {
+  const { closeHandler } = props;
+
+  return (
+    <Modal theme={"COACHMARK"} closeHandler={closeHandler}>
+      <St.Container>
+        <St.Contents>
+          새로 생긴 기능들을
+          <br />
+          확인해보세요
+        </St.Contents>
+        <St.ImageWrapper>
+          <St.ButtonWrapper>
+            <IcMenuBtn isLighted={true} />
+          </St.ButtonWrapper>
+          <St.ButtonWrapper>
+            <St.IconWrapper>
+              <IcShareBtn isLighted={true} />
+            </St.IconWrapper>
+            <St.ButtonLabel>공유하기</St.ButtonLabel>
+          </St.ButtonWrapper>
+        </St.ImageWrapper>
+      </St.Container>
+    </Modal>
+  );
+}
