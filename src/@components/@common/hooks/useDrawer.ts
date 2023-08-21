ae78a796aec0d073c45ca7c67fc280dc9d5af1df
Eunhyung Choi <milkiss0414@naver.com>
2023-08-21 10:37:50 +0900

    [ fix ] mobile optimization

--- src/@components/@common/hooks/useDrawer.ts
+++ src/@components/@common/hooks/useDrawer.ts
@@ -32,25 +32,21 @@ export default function useDrawer(closeModal: () => void) {
     [dragStart.y, isDragging],
   );
 
-  const handleMouseUp = useCallback(
-    (e: React.MouseEvent<HTMLElement>) => {
-      const container = containerRef.current;
-      if (!container) return;
+  const handleMouseUp = useCallback(() => {
+    const container = containerRef.current;
+    if (!container) return;
 
-      if (yOffset > 150) {
-        container.style.transition = "transform 0.2s ease-in-out";
-        container.style.transform = "translateY(100%)";
-        setTimeout(() => {
-          closeModal();
-        }, 250);
-      } else {
-        console.log(yOffset);
-        container.style.transform = "translateY(0)";
-      }
-      setIsDragging(false);
-    },
-    [yOffset, closeModal],
-  );
+    if (yOffset > 150) {
+      container.style.transition = "transform 0.2s ease-in-out";
+      container.style.transform = "translateY(100%)";
+      setTimeout(() => {
+        closeModal();
+      }, 250);
+    } else {
+      container.style.transform = "translateY(0)";
+    }
+    setIsDragging(false);
+  }, [yOffset, closeModal]);
 
   /**
    * For Mobile
@@ -69,21 +65,14 @@ export default function useDrawer(closeModal: () => void) {
     (e: React.TouchEvent<HTMLElement>) => {
       if (!isDragging) return;
 
-      const yOffset = Math.max(0, e.touches[0].clientY - dragStart.y);
+      const offset = Math.max(0, e.touches[0].clientY - dragStart.y);
 
       const container = containerRef.current;
       if (!container) return;
-      container.style.transform = `translateY(${yOffset}px)`;
-
-      if (yOffset > 200) {
-        container.style.transition = "transform 0.2s ease-in-out";
-        container.style.transform = "translateY(100%)";
-        setTimeout(() => {
-          closeModal();
-        }, 300);
-      }
+      container.style.transform = `translateY(${offset}px)`;
+      setYOffset(offset);
     },
-    [closeModal, dragStart.y, isDragging],
+    [dragStart.y, isDragging],
   );
 
   return {
