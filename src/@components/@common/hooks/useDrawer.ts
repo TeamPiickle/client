b2d717178070d4197bc535c23afed64f32329839
Eunhyung Choi <milkiss0414@naver.com>
2023-08-21 10:32:38 +0900

    [ feat ] add drag bounce

--- src/@components/@common/hooks/useDrawer.ts
+++ src/@components/@common/hooks/useDrawer.ts
@@ -6,6 +6,7 @@ export default function useDrawer(closeModal: () => void) {
 
   const [isDragging, setIsDragging] = useState(false);
   const [dragStart, setDragStart] = useState({ y: 0 });
+  const [yOffset, setYOffset] = useState(0);
 
   const handleMouseDown = useCallback((e: React.MouseEvent<HTMLElement>) => {
     if (knobRef.current && knobRef.current.contains(e.target as Node)) {
@@ -20,26 +21,36 @@ export default function useDrawer(closeModal: () => void) {
     (e: React.MouseEvent<HTMLElement>) => {
       if (!isDragging) return;
 
-      const yOffset = Math.max(0, e.clientY - dragStart.y);
+      const offset = Math.max(0, e.clientY - dragStart.y);
 
       // 모달을 드래그한 만큼 이동
       const container = containerRef.current;
       if (!container) return;
-      container.style.transform = `translateY(${yOffset}px)`;
-      if (yOffset > 200) {
+      container.style.transform = `translateY(${offset}px)`;
+      setYOffset(offset);
+    },
+    [dragStart.y, isDragging],
+  );
+
+  const handleMouseUp = useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      const container = containerRef.current;
+      if (!container) return;
+
+      if (yOffset > 150) {
         container.style.transition = "transform 0.2s ease-in-out";
         container.style.transform = "translateY(100%)";
         setTimeout(() => {
           closeModal();
-        }, 300);
+        }, 250);
+      } else {
+        console.log(yOffset);
+        container.style.transform = "translateY(0)";
       }
+      setIsDragging(false);
     },
-    [closeModal, dragStart.y, isDragging],
+    [yOffset, closeModal],
   );
-
-  const handleMouseUp = () => {
-    setIsDragging(false);
-  };
 
   /**
    * For Mobile
