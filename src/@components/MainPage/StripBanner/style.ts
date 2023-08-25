ed45a87ed262e144d60c2ea828dfbd0c5c608bf7
NYeonK <skdus728@gmail.com>
2023-08-26 05:19:04 +0900

    [ design ] add event strip banner style

--- src/@components/MainPage/StripBanner/style.ts
+++ src/@components/MainPage/StripBanner/style.ts
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-import { ImgStripBanner } from "../../../asset/image";
-import { OriginImgStripBanner } from "../../../asset/image/origin";
+import { ImgEventStripBanner, ImgStripBanner } from "../../../asset/image";
+import { OriginImgEventStripBanner, OriginImgStripBanner } from "../../../asset/image/origin";
 
 export const Container = styled(Link)`
   width: 100%;
@@ -24,14 +24,37 @@ export const ContentWrapper = styled.div`
   text-align: left;
 `;
 
-export const Content = styled.p`
+export const Content = styled.p<{ islogin?: boolean }>`
   margin-bottom: 0.2rem;
 
   ${({ theme }) => theme.newFonts.footer1}
-  color: ${({ theme }) => theme.newColors.purple};
+  color: ${({ theme, islogin }) => (islogin ? theme.newColors.white : theme.newColors.purple)}
 `;
 
-export const SubContent = styled.p`
+export const SubContent = styled.p<{ islogin?: boolean }>`
   ${({ theme }) => theme.newFonts.caption2}
-  color: ${({ theme }) => theme.newColors.gray700};
+  color: ${({ theme, islogin }) => (islogin ? theme.newColors.white : theme.newColors.gray700)}
+`;
+
+export const EventContainer = styled(Link)`
+  width: 100%;
+  height: 7rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  margin-top: 4rem;
+
+  background-image: url(${ImgEventStripBanner});
+  background-image: image-set(
+    ${ImgEventStripBanner} type("image/webp"),
+    ${OriginImgEventStripBanner} type("image/png")
+  );
+  background-size: 100% 100%;
+`;
+
+export const EventContentWrapper = styled(ContentWrapper)`
+  padding: 0 0 0.8rem 8rem;
+  margin: auto;
 `;
