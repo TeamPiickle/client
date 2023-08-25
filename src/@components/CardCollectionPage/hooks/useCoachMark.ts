a6b0c95dc881e748f7f55a922473388fab5659f9
NYeonK <skdus728@gmail.com>
2023-08-26 06:42:09 +0900

    [ refactor ] edit coachMark key name

--- src/@components/CardCollectionPage/hooks/useCoachMark.ts
+++ src/@components/CardCollectionPage/hooks/useCoachMark.ts
@@ -1,15 +1,15 @@
 import { useEffect, useState } from "react";
 
-const COACH_MARK_SESSION_KEY = "coach-mark-shown";
+const COACH_MARK_LOCAL_KEY = "coach-mark-shown";
 
 export default function useCoachMark() {
   const [isOpened, setIsOpened] = useState(false);
 
   useEffect(() => {
-    const isCoachMarkShown = localStorage.getItem(COACH_MARK_SESSION_KEY);
+    const isCoachMarkShown = localStorage.getItem(COACH_MARK_LOCAL_KEY);
     if (!isCoachMarkShown || isOpened) {
       setIsOpened(true);
-      localStorage.setItem(COACH_MARK_SESSION_KEY, "true");
+      localStorage.setItem(COACH_MARK_LOCAL_KEY, "true");
     }
   }, []);
 
